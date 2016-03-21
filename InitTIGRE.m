c2b989e2d8c898236c6247a13c9111077d560f61
aabhca20 <aabhca20@EEPC-301.campus.bath.ac.uk>
2016-03-21 10:28:45 +0000

    Added Init, checked bp2

---
+++ InitTIGRE.m
@@ -0,0 +1,32 @@
+% Add tolbox folders
+addpath('.\Algorithms');
+addpath('.\Utilities');
+addpath('.\Utilities\Quality_measures');
+addpath(genpath('.\Test_data'));
+
+% different arch versions
+if ~isempty(strfind(computer('arch'),'64'))
+    addpath('.\Mex_files\win64');
+else
+    addpath('.\Mex_files\win32');
+end
+
+addpath('.\Demos');
+
+% Perceptually uniform colormaps
+addpath('.\Colormaps');
+% Add third party tools from FEX
+addpath('.\Third_party_tools\arrow3d'); % 3D shepp-Logan
+addpath('.\Third_party_tools\sec2hours'); 
+addpath('.\Third_party_tools\readMHD'); 
+
+
+[user, sys]=memory;
+
+if sys.PhysicalMemory.Total<9000000000 % 8Gb
+   warning('Your Computer has 8Gb or less of RAM memory. Using image sizes of higher than 512^3 is not recomended (most likely not possible)') 
+end
+
+if sys.PhysicalMemory.Total<2500000000 % 2Gb
+   warning('Your Computer has 2Gb or less of RAM memory. Using image sizes of higher than 256^3 is not recomended (most likely not possible)') 
+end
