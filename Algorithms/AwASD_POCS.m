3a27cffde6685c41dd445b50d990c9659ff3e67c
Daveelvt <manasavee.lohvithee@gmail.com>
2016-06-21 16:22:31 +0100

    Add explanation in function AwASD_POCS

--- Algorithms/AwASD_POCS.m
+++ Algorithms/AwASD_POCS.m
@@ -1,16 +1,18 @@
 function [ fres ] = AwASD_POCS(proj,geo,angles,maxiter,varargin)
-%ASD_POCS Solves the ASD_POCS total variation constrained image in 3D
-% tomography. 
-% 
-%   ASD_POCS(PROJ,GEO,ALPHA,NITER) solves the reconstruction problem
+%AwASD_POCS Solves the 3D tomography problem using the adaptive-weighted
+%ASD_POCS algorithm which extends from the method ASD_POCS available in the
+%TIGRE toolbox by adding weight equation to better preserve the edge of the
+%reconstructed image
+%
+%   AwASD_POCS(PROJ,GEO,ALPHA,NITER) added adaptive-weighted solves the reconstruction problem
 %   using the projection data PROJ taken over ALPHA angles, corresponding
 %   to the geometry descrived in GEO, using NITER iterations.
 %
-%   ASD_POCS(PROJ,GEO,ALPHA,NITER,OPT,VAL,...) uses options and values for solving. The
+%   AwASD_POCS(PROJ,GEO,ALPHA,NITER,OPT,VAL,...) uses options and values for solving. The
 %   possible options in OPT are:
 %
 %
-%   'lambda':      Sets the value of the hyperparameter for the SART iterations. 
+%   'lambda':      Sets the value of the hyperparameter for the SART iterations.
 %                  Default is 1
 %
 %   'lambdared':   Reduction of lambda.Every iteration
@@ -20,10 +22,10 @@ function [ fres ] = AwASD_POCS(proj,geo,angles,maxiter,varargin)
 %                  iteration. Default is 20
 %
 %   'alpha':       Defines the TV hyperparameter. default is 0.002
-% 
+%
 %   'alpha_red':   Defines the reduction rate of the TV hyperparameter
 %
-%   'Ratio':       The maximum allowed image/TV update ration. If the TV 
+%   'Ratio':       The maximum allowed image/TV update ration. If the TV
 %                  update changes the image more than this, the parameter
 %                  will be reduced.default is 0.95
 %   'maxL2err'     Maximum L2 error to accept an image as valid. This
@@ -98,6 +100,7 @@ while ~stop_criteria %POCS
     end
     f0=f;
     
+    %Compute the local image-intensity
     Gx=diff(f,1,1);
     Gy=diff(f,1,2);
     Gz=diff(f,1,3);
@@ -106,51 +109,70 @@ while ~stop_criteria %POCS
     Gx=cat(1,zeros(size(Gx(1,:,:))),Gx);
     Gy=cat(2,zeros(size(Gy(:,1,:))),Gy);
     Gz=cat(3,zeros(size(Gz(:,:,1))),Gz);
-
+    
+    %Define a parameter delta which controls the amount of smoothing that
+    %we would like to apply to those pixels at edges.
+    
+    %Need to explain how to define parameter: delta
+    
     delta=-0.00055;
-%      delta=0.0002;
-%--------------------------------------------------------------------------            
-            weightx=exp(-(Gx./delta).^2);
-            weighty=exp(-(Gy./delta).^2);
-            weightz=exp(-(Gz./delta).^2);
-%-------------------------------------------------------------------------- 
-
-%                weightx=1/(1+((Gx.^2)/(delta^2)));
-%                weighty=1/(1+((Gy.^2)/(delta^2)));
-%                weightz=1/(1+((Gz.^2)/(delta^2)));
-
-
-%--------------------------------------------------------------------------            
-% %             if abs(Gx)<=delta
-%             weightx=(1/2)*(1-(Gx./delta).^2).^2;
-% %             else
-% %             weightx=0;
-% %             end
-%             
-% %              if abs(Gy)<=delta
-%             weighty=(1/2)*(1-(Gy./delta).^2).^2;
-% %             else
-% %             weighty=0;
-% %              end
-%             
-% %             if abs(Gz)<=delta
-%             weightz=(1/2)*(1-(Gz./delta).^2).^2;
-% %             else
-% %             weightz=0;
-% %             end
-%--------------------------------------------------------------------------
-
+    % delta=0.0002;
+    %--------------------------------------------------------------------------
+    % Weighting equation#1 (An exponential function of the component of the local image-intensity gradient vector)
+    
+    %Large delta is not able to differentiate image gradients at different
+    %pixels
+    
+    %Small delta gives low weights to almost every pixel, making the norm
+    %inefficient in removing noise or streaking artifacts.
+    %ref: doi:10.1088/0031-9155/56/17/011
+    
+    
+    weightx=exp(-(Gx./delta).^2);
+    weighty=exp(-(Gy./delta).^2);
+    weightz=exp(-(Gz./delta).^2);
+    %--------------------------------------------------------------------------
+    %Weighting equation#2 (One of the two different edge stopping g(.)functions in Perona and Malik anisotropic diffusion equation )
+    
+    
+    %                weightx=1/(1+((Gx.^2)/(delta^2)));
+    %                weighty=1/(1+((Gy.^2)/(delta^2)));
+    %                weightz=1/(1+((Gz.^2)/(delta^2)));
+    
+    
+    %--------------------------------------------------------------------------
+     %Weighting equation#3 (Robust Anisotropic Diffusion)
+    
+    % %             if abs(Gx)<=delta
+    %             weightx=(1/2)*(1-(Gx./delta).^2).^2;
+    % %             else
+    % %             weightx=0;
+    % %             end
+    %
+    % %              if abs(Gy)<=delta
+    %             weighty=(1/2)*(1-(Gy./delta).^2).^2;
+    % %             else
+    % %             weighty=0;
+    % %              end
+    %
+    % %             if abs(Gz)<=delta
+    %             weightz=(1/2)*(1-(Gz./delta).^2).^2;
+    % %             else
+    % %             weightz=0;
+    % %             end
+    %--------------------------------------------------------------------------
+    
     %  TV MINIMIZATION
     % =========================================================================
     %  Call GPU to minimize TV
-%     f=minimizeTV(f0,dtvg,ng);    %   This is the MATLAB CODE, the functions are sill in the library, but CUDA is used nowadays
-                                            for ii=1:ng
-%                                                 Steepest descend of TV norm
-                                                tv(ng*(iter-1)+ii)=im3Dnorm(f,'TV','forward');
-                                                df=weighted_gradientTVnorm(f,weightx,weighty,weightz);
-                                                df=df./im3Dnorm(df,'L2');
-                                                f=f-dtvg.*df;
-                                            end
+    %     f=minimizeTV(f0,dtvg,ng);    %   This is the MATLAB CODE, the functions are sill in the library, but CUDA is used nowadays
+    for ii=1:ng
+        % Steepest descend of TV norm
+        tv(ng*(iter-1)+ii)=im3Dnorm(f,'TV','forward');
+        df=weighted_gradientTVnorm(f,weightx,weighty,weightz);
+        df=df./im3Dnorm(df,'L2');
+        f=f-dtvg.*df;
+    end
     
     % update parameters
     % ==========================================================================
@@ -184,7 +206,7 @@ while ~stop_criteria %POCS
         expected_time=toc*maxiter;
         disp('ADS-POCS');
         disp(['Expected duration  :    ',secs2hms(expected_time)]);
-        disp(['Exected finish time:    ',datestr(datetime('now')+seconds(expected_time))]);
+        disp(['Expected finish time:    ',datestr(datetime('now')+seconds(expected_time))]);
         disp('');
     end
     
@@ -281,8 +303,8 @@ for ii=1:length(opts)
             else
                 alpha_red=val;
             end
-            %  Maximum update ratio 
-            %  =========================================================================            
+            %  Maximum update ratio
+            %  =========================================================================
         case 'Ratio'
             if default
                 rmax=0.95;
@@ -290,16 +312,16 @@ for ii=1:length(opts)
                 rmax=val;
             end
             %  Maximum L2 error to have a "good image"
-            %  =========================================================================       
+            %  =========================================================================
         case 'maxL2err'
             if default
-               epsilon=im3Dnorm(FDK(proj,geo,angles))*0.2; %heuristic
+                epsilon=im3Dnorm(FDK(proj,geo,angles))*0.2; %heuristic
             else
-               epsilon=val;
+                epsilon=val;
             end
         otherwise
-             error('CBCT:ASD_POCS:InvalidInput',['Invalid input name:', num2str(opt),'\n No such option in ASD_POCS()']);
-    
+            error('CBCT:ASD_POCS:InvalidInput',['Invalid input name:', num2str(opt),'\n No such option in ASD_POCS()']);
+            
     end
 end
 
