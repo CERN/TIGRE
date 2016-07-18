39fff5caee4f7befc40e4ac57df82ef934226916
AnderBiguri <aabhca20@EEPC-301.campus.bath.ac.uk>
2016-07-18 09:19:27 +0100

    Add missing Licenses
    to possonrandom, MLEM and areTheseToolboxesInstalled

--- Algorithms/MLEM.m
+++ Algorithms/MLEM.m
@@ -5,6 +5,22 @@ function [res]=MLEM(proj,geo,angles,niter,varargin)
 %   MLEM(PROJ,GEO,ALPHA,NITER) solves the reconstruction problem
 %   using the projection data PROJ taken over ALPHA angles, corresponding
 %   to the geometry descrived in GEO, using NITER iterations.
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+% This file is part of the TIGRE Toolbox
+% 
+% Copyright (c) 2015, University of Bath and 
+%                     CERN-European Organization for Nuclear Research
+%                     All rights reserved.
+%
+% License:            Open Source under BSD. 
+%                     See the full license at
+%                     https://github.com/CERN/TIGRE/license.txt
+%
+% Contact:            tigre.toolbox@gmail.com
+% Codes:              https://github.com/CERN/TIGRE/
+% Coded by:           Ander Biguri 
+%--------------------------------------------------------------------------
 
 res=ones(geo.nVoxel.','single');
 W=Atb(ones([geo.nDetector.' numel(angles)],'single'),geo,angles);
--- Utilities/poissonrandom.m
+++ Utilities/poissonrandom.m
@@ -3,6 +3,22 @@ function km=poissonrandom(lambda)
 % Donald Knuth's poisson random generator
 
 % This sucks bananas because its O(lambda) and we want lambda~=10000...
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+% This file is part of the TIGRE Toolbox
+% 
+% Copyright (c) 2015, University of Bath and 
+%                     CERN-European Organization for Nuclear Research
+%                     All rights reserved.
+%
+% License:            Open Source under BSD. 
+%                     See the full license at
+%                     https://github.com/CERN/TIGRE/license.txt
+%
+% Contact:            tigre.toolbox@gmail.com
+% Codes:              https://github.com/CERN/TIGRE/
+% Coded by:           Ander Biguri
+%--------------------------------------------------------------------------
 
 km=zeros(size(lambda));
 for ii=1:numel(lambda)
