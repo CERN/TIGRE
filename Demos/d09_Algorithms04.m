aec6ed24d6e97bafffead57126d2597fe221574b
AnderBiguri <aabhca20@bath.ac.uk>
2016-11-28 10:19:25 +0000

    Remove test code

--- Demos/d09_Algorithms04.m
+++ Demos/d09_Algorithms04.m
@@ -38,11 +38,11 @@ close all;
 geo.DSD = 1536;                             % Distance Source Detector      (mm)
 geo.DSO = 1000;                             % Distance Source Origin        (mm)
 % Detector parameters
-geo.nDetector=[512; 512]/4;					% number of pixels              (px)
+geo.nDetector=[512; 512];					% number of pixels              (px)
 geo.dDetector=[0.8; 0.8]; 					% size of each pixel            (mm)
 geo.sDetector=geo.nDetector.*geo.dDetector; % total size of the detector    (mm)
 % Image parameters
-geo.nVoxel=[128;128;128]/2;                   % number of voxels              (vx)
+geo.nVoxel=[128;128;128];                   % number of voxels              (vx)
 geo.sVoxel=[256;256;256];                   % total size of the image       (mm)
 geo.dVoxel=geo.sVoxel./geo.nVoxel;          % size of each voxel            (mm)
 % Offsets
@@ -50,7 +50,6 @@ geo.offOrigin =[0;0;0];                     % Offset of image from origin   (mm)
 geo.offDetector=[0; 0];                     % Offset of Detector            (mm)
 % Auxiliary 
 geo.accuracy=0.5;                           % Accuracy of FWD proj          (vx/sample)
-geo.offOrigin =[rand(3,30)];                     % Offset of image from origin   (mm)              
 
 %% Load data and generate projections 
 % see previous demo for explanation
