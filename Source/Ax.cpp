0d16528638aba5afce838e1f59419c4143fd72af
AnderBiguri <aabhca20@EEPC-301.campus.bath.ac.uk>
2017-04-28 10:01:56 +0100

    Fix bug in projection and projection demo

--- Source/Ax.cpp
+++ Source/Ax.cpp
@@ -86,7 +86,7 @@ void mexFunction(int  nlhs , mxArray *plhs[],
             mexErrMsgIdAndTxt( "CBCT:MEX:Ax:InvalidInput","4rd input shoudl be either 'interpolated' or 'ray-voxel'");
         else
             // If its not ray-voxel, its "interpolated"
-            if (strcmp(krylov,"ray-voxel"))
+            if (!strcmp(krylov,"ray-voxel"))
                 rayvoxel=true;
     }
     ///////////////////////// 3rd argument: angle of projection.
