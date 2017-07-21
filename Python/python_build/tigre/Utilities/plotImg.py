c6777b45d3e452d6a48da180c42eaa7f096f9057
reuben481 <rllindroos@gmail.com>
2017-07-21 16:46:55 +0200

    Modified plotImg docs and dim vals.

--- Python/python_build/tigre/Utilities/plotImg.py
+++ Python/python_build/tigre/Utilities/plotImg.py
@@ -15,54 +15,38 @@ class plotImg:
      '---------- \n'
      'cube : Any 3D numpy array \n'
      '\n'
-     'dim : ("X","Y","Z","x","y","z"), optional \n'
-     '       default is "Z"\n'
-     '       NOTE: string arguments!'
+     'dim : int, describes axis of progression.'
      '\n'
      'slice: int, optional\n'
      '     returns page of matrix according to index\n'
+     'slider: bool, optional\n'
+     '     returns interactive plot'
      'Examples:\n'
      '---------\n'
      'a=np.ones([3,3,3])\n'
      'plotImg(a)\n'
-     '>>>returns plot along dim Z\n'
-     'plotImg(a,dim="X")\n'
+     '>>>returns plot along dim 0\n'
+     'plotImg(a,dim=0)\n'
      '>>>returns plot along dim X\n')
 
-    def __init__(self, cube, dim=None, slice=None,slider=False):
+    def __init__(self, cube, dim=None, slice=None,slider=False,**kwargs):
         self.cube = cube
         self.dim = dim
         self.slice = slice
         self.slider=slider
-        self.dimint = None  # keeps track of what dim
-        self.dimlist = ['X', 'Y', 'Z', 'x', 'y', 'z', None]  # accepted parameters for dim
+        self.dimint = dim
+        self.kwargs=kwargs
         if self.slider:
             self.cube_show_slider()
             return 
         if self.slice is None:
-            self.run()
+            self.run_plot()
         if self.slice is not None:
             self.slicer()
-
-
-    def run(self):
-        if self.dim not in self.dimlist:
-            raise NameError('check inputs for dim, should be string.')
-
-        if self.dim in [None, 'Z', 'z']:
-            self.dimint = 2
-            self.dimlist = ['X', 'Y', 'Z']
-            self.run_plot()
-        if self.dim in ['X', 'x']:
-            self.dimint = 0
-            self.dimlist = ['Y', 'Z', 'X']
-            self.run_plot()
-        if self.dim in ['Y', 'y']:
-            self.dimint = 1
-            self.dimlist = ['X', 'Z', 'Y']
-            self.run_plot()
-
     def run_plot(self):
+        if self.dimint is None:
+            self.dimint = 0
+
         plt.ion()
         min_val = np.amin(self.cube)
         max_val = np.amax(self.cube)
@@ -80,9 +64,7 @@ class plotImg:
                            vmax=max_val)
             plt.gca().get_xaxis().set_ticks([])
             plt.gca().get_yaxis().set_ticks([])
-            plt.xlabel(self.dimlist[0])
-            plt.ylabel(self.dimlist[1])
-            plt.title(self.dimlist[2] + ':' + str(i))
+            plt.title('slice' + ':' + str(i))
 
             plt.colorbar()
 
@@ -106,8 +88,13 @@ class plotImg:
 
         Extra keyword arguments are passed to imshow
         """
+        kwargs.update(self.kwargs)
         cube=self.cube
-        axis=0
+        if self.dimint is None:
+            axis=0
+        else:
+            axis=self.dimint
+
 
         # check dim
         if not cube.ndim == 3:
