13f488c1932678501b4526af87ffccac6dc369ed
Lindroos R <rl1u18@soton.ac.uk>
2018-09-28 18:58:20 +0100

    Compiler for windows in progress

--- Python/python_build/setup.py
+++ Python/python_build/setup.py
@@ -6,6 +6,7 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import subprocess
 import numpy
+from sys import platform
 
 
 # Code from https://github.com/rmcgibbo/npcuda-example/blob/master/cython/setup.py
@@ -30,25 +31,42 @@ def locate_cuda():
     Starts by looking for the CUDAHOME env variable. If not found, everything
     is based on finding 'nvcc' in the PATH.
     """
+    if platform == "win32":
+        if 'CUDA_PATH' in os.environ:
+            home = os.environ['CUDA_PATH']
+            nvcc = pjoin(home, 'bin', 'nvcc.exe')
+        else:
+            raise EnvironmentError('CUDA_PATH could not be found in your environment variables.')
+        home = os.path.dirname(os.path.dirname(nvcc))
+        cudaconfig = {'home': home, 'nvcc': nvcc,
+                      'lib': pjoin(home, 'lib'),
+                      'x64': pjoin(home,'lib', 'x64')}
+        for k, v in cudaconfig.iteritems():
+            if not os.path.exists(v):
+                raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
+
+
+    #defaulting to linux until this can be tested.
+    else:
 
     # first check if the CUDAHOME env variable is in use
-    if 'CUDAHOME' in os.environ:
-        home = os.environ['CUDAHOME']
-        nvcc = pjoin(home, 'bin', 'nvcc')
-    else:
-        # otherwise, search the PATH for NVCC
-        nvcc = find_in_path('nvcc', os.environ['PATH'])
-        if nvcc is None:
-            raise EnvironmentError('The nvcc binary could not be located in your $PATH. '
-                                   'Either add it to your path, or set $CUDAHOME')
-        home = os.path.dirname(os.path.dirname(nvcc))
+        if 'CUDAHOME' in os.environ:
+            home = os.environ['CUDAHOME']
+            nvcc = pjoin(home, 'bin', 'nvcc')
+        else:
+            # otherwise, search the PATH for NVCC
+            nvcc = find_in_path('nvcc', os.environ['PATH'])
+            if nvcc is None:
+                raise EnvironmentError('The nvcc binary could not be located in your $PATH. '
+                                       'Either add it to your path, or set $CUDAHOME')
+            home = os.path.dirname(os.path.dirname(nvcc))
 
-    cudaconfig = {'home': home, 'nvcc': nvcc,
-                  'include': pjoin(home, 'include'),
-                  'lib64': pjoin(home, 'lib64')}
-    for k, v in cudaconfig.iteritems():
-        if not os.path.exists(v):
-            raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
+        cudaconfig = {'home': home, 'nvcc': nvcc,
+                      'include': pjoin(home, 'include'),
+                      'lib64': pjoin(home, 'lib64')}
+        for k, v in cudaconfig.iteritems():
+            if not os.path.exists(v):
+                raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
 
     return cudaconfig
 
