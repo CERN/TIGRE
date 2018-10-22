950d5ad72e0e5b4025f12ed0558eca5c3ca142c6
reuben lindroos <reuben@dhcp-ASPIRE.soton.ac.uk>
2018-10-22 16:21:48 +0100

    modified architecture. added compute capability in setup.py

--- Python/setup.py
+++ Python/setup.py
@@ -113,7 +113,11 @@ Ax_ext = Extension('_Ax',
                    # we're only going to use certain compiler args with nvcc and not with gcc
                    # the implementation of this trick is in customize_compiler() below
                    extra_compile_args={'gcc': [],
-                                        'nvcc': ['-arch=sm_20', '--ptxas-options=-v', '-c',
+                                        'nvcc': ['-gencode=arch=compute_20,code=sm_20',
+                                                 '-gencode=arch=compute_30,code=sm_30',
+                                                 #'-gencode=arch=compute_40,code=sm_40',
+                                                 #'-gencode=arch=compute_50,code=sm_50',
+                                                 '--ptxas-options=-v', '-c',
                                                  '--compiler-options', "'-fPIC'"]},
                    include_dirs=[numpy_include, CUDA['include'], 'Source'])
 
@@ -130,8 +134,12 @@ Atb_ext = Extension('_Atb',
                     # we're only going to use certain compiler args with nvcc and not with gcc
                     # the implementation of this trick is in customize_compiler() below
                     extra_compile_args={'gcc': [],
-                                         'nvcc': ['-arch=sm_20', '--ptxas-options=-v', '-c',
-                                                  '--compiler-options', "'-fPIC'"]},
+                                         'nvcc': ['-gencode=arch=compute_20,code=sm_20',
+                                                 '-gencode=arch=compute_30,code=sm_30',
+                                                 #'-gencode=arch=compute_40,code=sm_40',
+                                                 #'-gencode=arch=compute_50,code=sm_50',
+                                                 #'--ptxas-options=-v', '-c',
+                                                 '--compiler-options', "'-fPIC'"]},
                     include_dirs=[numpy_include, CUDA['include'], 'tigre/Source'])
 tvdenoising_ext = Extension('_tvdenoising',
                     sources=(['tigre/Source/voxel_backprojection.cu', 'tigre/Source/tvdenoising.cu',
@@ -145,8 +153,12 @@ tvdenoising_ext = Extension('_tvdenoising',
                     # we're only going to use certain compiler args with nvcc and not with gcc
                     # the implementation of this trick is in customize_compiler() below
                     extra_compile_args={'gcc': [],
-                                         'nvcc': ['-arch=sm_20', '--ptxas-options=-v', '-c',
-                                                  '--compiler-options', "'-fPIC'"]},
+                                         'nvcc':['-gencode=arch=compute_20,code=sm_20',
+                                                 '-gencode=arch=compute_30,code=sm_30',
+                                                 #'-gencode=arch=compute_40,code=sm_40',
+                                                 #'-gencode=arch=compute_50,code=sm_50',
+                                                 #'--ptxas-options=-v', '-c',
+                                                 '--compiler-options', "'-fPIC'"]},
                     include_dirs=[numpy_include, CUDA['include'], 'Source'])
 
 # run the customize_compiler
