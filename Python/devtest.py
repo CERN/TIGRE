2d0cbee41c4f9030cdf08d904814580ec44cdf6f
reuben lindroos <reuben@dhcp-ASPIRE.soton.ac.uk>
2019-01-15 17:03:10 +0000

    setup.py now adds header file for distribution.

--- Python/devtest.py
+++ Python/devtest.py
@@ -1,40 +1,55 @@
 from __future__ import print_function
+import os
+import sys
+"""
 import tigre
-import numpy as np
-import tigre.demos.Test_data.data_loader as data_loader
+#import numpy as np
+#import tigre.demos.Test_data.data_loader as data_loader
 from tigre.algorithms.conjugate_gradient_algorithms import CGLS
 from tigre.algorithms.pocs_algorithms import ASD_POCS
 from tigre.algorithms.art_family_algorithms import SART
 from matplotlib import pyplot as plt
-import tigre
+#import tigre
 import tigre.algorithms as algs
 from tigre.utilities.Ax import Ax
+"""
 
 # ---------------GEOMETRY---------------------------
 
-geo = tigre.geometry(mode='parallel',nVoxel = np.array([64,64,64],dtype=np.float32))
-source_img = data_loader.load_head_phantom(number_of_voxels=geo.nVoxel)
+#geo = tigre.geometry(mode='parallel',nVoxel = np.array([64,64,64],dtype=np.float32))
+#source_img = data_loader.load_head_phantom(number_of_voxels=geo.nVoxel)
 
 
 # ---------------------ANGLES-------------------------
 
-angles_1 = np.linspace(0, 2 * np.pi, 100, dtype=np.float32)
-angles_2 = np.ones((100), dtype=np.float32) * np.array(np.pi / 4, dtype=np.float32)
-angles_3 = np.zeros((100), dtype=np.float32)
-angles = np.vstack((angles_1, angles_3, angles_3)).T
+#angles_1 = np.linspace(0, 2 * np.pi, 100, dtype=np.float32)
+#angles_2 = np.ones((100), dtype=np.float32) * np.array(np.pi / 4, dtype=np.float32)
+#angles_3 = np.zeros((100), dtype=np.float32)
+#angles = np.vstack((angles_1, angles_3, angles_3)).T
 
 # --------------------PROJECTION----------------------
-from tigre.demos.__test import test
-test()
-"""
-# --------------------BACK PROJECTION ----------------
-#res = algs.awasd_pocs(proj_1,geo,angles,10,**dict(blocksize=20))
-#res = algs.ossart(proj_1,geo,angles,15,**dict(blocksize=12))
-res = algs.awasd_pocs(proj_1,geo,angles,5)
+def include_headers(filename_list, sdist=False):
+    """add hpp and h files to list if sdist is called"""
+    c_extensions = ['.cu',".c", ".C", ".cc", ".cpp", ".cxx", ".c++"]
+    header_list = []
+    for filename in filename_list:
+        if sdist:
+            header = list(os.path.splitext(filename))
+            if header[1] in c_extensions:
+                header[1] = '.hpp'
+                header_list.append(''.join(header))
+    if sdist:
+        filename_list.append('tigre/Source/types.hpp')
+    return filename_list + header_list
 
-# ---------------PLOTS------------------------------
 
-plt.matshow(abs(new_head[32] - source_img[32]))
-plt.colorbar()
-plt.show()
-"""
+
+
+
+test = ['tigre/Source/projection.cpp',
+'tigre/Source/Siddon_projection.cu', 'tigre/Source/Siddon_projection_parallel.cu',
+'tigre/Source/ray_interpolated_projection.cu', 'tigre/Source/ray_interpolated_projection_parallel.cu',
+'tigre/Source/_types.pxd',
+'tigre/Source/_Ax.pyx']
+
+print(include_headers(test,sdist=True))
--- Python/setup.py
+++ Python/setup.py
@@ -1,23 +1,24 @@
 from __future__ import absolute_import
-import  os
+import os
 from os.path import join as pjoin
-from setuptools import setup, find_packages
-from distutils.extension import Extension
+from setuptools import setup, find_packages, Extension
+# from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import subprocess
 import numpy
-
+import sys
 
 # Code from https://github.com/rmcgibbo/npcuda-example/blob/master/cython/setup.py
 compute_capability_args = ['-gencode=arch=compute_20,code=sm_20',
                            '-gencode=arch=compute_30,code=sm_30',
                            '-gencode=arch=compute_37,code=sm_37',
-                           #'-gencode=arch=compute_52,code=sm_52',
-                           #'-gencode=arch=compute_60,code=sm_60',
-                           #'-gencode=arch=compute_61,code=sm_61',
-                           #'-gencode=arch=compute_70,code=sm_70', #untested
+                           # '-gencode=arch=compute_52,code=sm_52',
+                           # '-gencode=arch=compute_60,code=sm_60',
+                           # '-gencode=arch=compute_61,code=sm_61',
+                           # '-gencode=arch=compute_70,code=sm_70', #untested
                            '--ptxas-options=-v', '-c',
                            '--compiler-options', "'-fPIC'"]
+
 
 def find_in_path(name, path):
     "Find a file in a search path"
@@ -47,9 +48,12 @@ def locate_cuda():
         # otherwise, search the PATH for NVCC
         nvcc = find_in_path('nvcc', os.environ['PATH'])
         if nvcc is None:
-            raise EnvironmentError('The nvcc binary could not be located in your $PATH. '
+            print('WARNING: The nvcc binary could not be located in your $PATH. '
                                    'Either add it to your path, or set $CUDAHOME')
-        home = os.path.dirname(os.path.dirname(nvcc))
+            home = '/usr/local/cuda'
+            nvcc = pjoin(home,'bin','nvcc')
+        else:
+            home = os.path.dirname(os.path.dirname(nvcc))
 
     cudaconfig = {'home': home, 'nvcc': nvcc,
                   'include': pjoin(home, 'include'),
@@ -107,12 +111,30 @@ def customize_compiler_for_nvcc(self):
     # inject our redefined _compile method into the class
     self._compile = _compile
 
+
+def include_headers(filename_list, sdist=False):
+    """add hpp and h files to list if sdist is called"""
+    c_extensions = ['.cu', ".c", ".C", ".cc", ".cpp", ".cxx", ".c++"]
+    header_list = []
+    for filename in filename_list:
+        if sdist:
+            header = list(os.path.splitext(filename))
+            if header[1] in c_extensions:
+                header[1] = '.hpp'
+                header_list.append(''.join(header))
+    if sdist:
+        filename_list+=['tigre/Source/types_TIGRE.hpp','tigre/Source/types.hpp']
+    return filename_list + header_list
+
+
 Ax_ext = Extension('_Ax',
-                   sources=(['tigre/Source/projection.cpp',
-                             'tigre/Source/Siddon_projection.cu', 'tigre/Source/Siddon_projection_parallel.cu',
-                             'tigre/Source/ray_interpolated_projection.cu', 'tigre/Source/ray_interpolated_projection_parallel.cu',
-                             'tigre/Source/_types.pxd',
-                             'tigre/Source/_Ax.pyx']),
+                   sources=(include_headers(['tigre/Source/projection.cpp',
+                                             'tigre/Source/Siddon_projection.cu',
+                                             'tigre/Source/Siddon_projection_parallel.cu',
+                                             'tigre/Source/ray_interpolated_projection.cu',
+                                             'tigre/Source/ray_interpolated_projection_parallel.cu',
+                                             'tigre/Source/_types.pxd',
+                                             'tigre/Source/_Ax.pyx'], sdist=(sys.argv[1] == "sdist"))),
                    library_dirs=[CUDA['lib64']],
                    libraries=['cudart'],
                    language='c++',
@@ -121,18 +143,18 @@ Ax_ext = Extension('_Ax',
                    # we're only going to use certain compiler args with nvcc and not with gcc
                    # the implementation of this trick is in customize_compiler() below
                    extra_compile_args={'gcc': [],
-                                        'nvcc': compute_capability_args},
+                                       'nvcc': compute_capability_args},
                    include_dirs=[numpy_include, CUDA['include'], 'Source'])
 
 Atb_ext = Extension('_Atb',
-                    sources=(['tigre/Source/voxel_backprojection.cu',
-                              'tigre/Source/voxel_backprojection_spherical.cu',
-                              'tigre/Source/voxel_backprojection2.cu',
-                              'tigre/Source/voxel_backprojection2_spherical.cu',
-                              'tigre/Source/voxel_backprojection_parallel.cu',
-                              'tigre/Source/voxel_backprojection_parallel_spherical.cu',
-                              'tigre/Source/_types.pxd',
-                              'tigre/Source/_Atb.pyx']),
+                    sources=(include_headers(['tigre/Source/voxel_backprojection.cu',
+                                              'tigre/Source/voxel_backprojection_spherical.cu',
+                                              'tigre/Source/voxel_backprojection2.cu',
+                                              'tigre/Source/voxel_backprojection2_spherical.cu',
+                                              'tigre/Source/voxel_backprojection_parallel.cu',
+                                              'tigre/Source/voxel_backprojection_parallel_spherical.cu',
+                                              'tigre/Source/_types.pxd',
+                                              'tigre/Source/_Atb.pyx'], sdist=(sys.argv[1] == "sdist"))),
                     library_dirs=[CUDA['lib64']],
                     libraries=['cudart'],
                     language='c++',
@@ -141,51 +163,52 @@ Atb_ext = Extension('_Atb',
                     # we're only going to use certain compiler args with nvcc and not with gcc
                     # the implementation of this trick is in customize_compiler() below
                     extra_compile_args={'gcc': [],
-                                         'nvcc': compute_capability_args},
+                                        'nvcc': compute_capability_args},
                     include_dirs=[numpy_include, CUDA['include'], 'tigre/Source'])
 tvdenoising_ext = Extension('_tvdenoising',
-                    sources=(['tigre/Source/voxel_backprojection.cu', 'tigre/Source/tvdenoising.cu',
-                              'tigre/Source/_types.pxd',
-                              'tigre/Source/_tvdenoising.pyx']),
-                    library_dirs=[CUDA['lib64']],
-                    libraries=['cudart'],
-                    language='c++',
-                    runtime_library_dirs=[CUDA['lib64']],
-                    # this syntax is specific to this build system
-                    # we're only going to use certain compiler args with nvcc and not with gcc
-                    # the implementation of this trick is in customize_compiler() below
-                    extra_compile_args={'gcc': [],
-                                         'nvcc':compute_capability_args},
-                    include_dirs=[numpy_include, CUDA['include'], 'Source'])
+                            sources=(
+                                include_headers(['tigre/Source/voxel_backprojection.cu', 'tigre/Source/tvdenoising.cu',
+                                                 'tigre/Source/_types.pxd',
+                                                 'tigre/Source/_tvdenoising.pyx'], sdist=(sys.argv[1] == "sdist"))),
+                            library_dirs=[CUDA['lib64']],
+                            libraries=['cudart'],
+                            language='c++',
+                            runtime_library_dirs=[CUDA['lib64']],
+                            # this syntax is specific to this build system
+                            # we're only going to use certain compiler args with nvcc and not with gcc
+                            # the implementation of this trick is in customize_compiler() below
+                            extra_compile_args={'gcc': [],
+                                                'nvcc': compute_capability_args},
+                            include_dirs=[numpy_include, CUDA['include'], 'Source'])
 minTV_ext = Extension('_minTV',
-                    sources=(['tigre/Source/POCS_TV.cu',
-                              'tigre/Source/_types.pxd',
-                              'tigre/Source/_minTV.pyx']),
-                    library_dirs=[CUDA['lib64']],
-                    libraries=['cudart'],
-                    language='c++',
-                    runtime_library_dirs=[CUDA['lib64']],
-                    # this syntax is specific to this build system
-                    # we're only going to use certain compiler args with nvcc and not with gcc
-                    # the implementation of this trick is in customize_compiler() below
-                    extra_compile_args={'gcc': [],
-                                         'nvcc':compute_capability_args},
-                    include_dirs=[numpy_include, CUDA['include'], 'Source'])
+                      sources=(include_headers(['tigre/Source/POCS_TV.cu',
+                                                'tigre/Source/_types.pxd',
+                                                'tigre/Source/_minTV.pyx'], sdist=(sys.argv[1] == "sdist"))),
+                      library_dirs=[CUDA['lib64']],
+                      libraries=['cudart'],
+                      language='c++',
+                      runtime_library_dirs=[CUDA['lib64']],
+                      # this syntax is specific to this build system
+                      # we're only going to use certain compiler args with nvcc and not with gcc
+                      # the implementation of this trick is in customize_compiler() below
+                      extra_compile_args={'gcc': [],
+                                          'nvcc': compute_capability_args},
+                      include_dirs=[numpy_include, CUDA['include'], 'Source'])
 
 AwminTV_ext = Extension('_AwminTV',
-                    sources=(['tigre/Source/POCS_TV2.cu',
-                              'tigre/Source/_types.pxd',
-                              'tigre/Source/_AwminTV.pyx']),
-                    library_dirs=[CUDA['lib64']],
-                    libraries=['cudart'],
-                    language='c++',
-                    runtime_library_dirs=[CUDA['lib64']],
-                    # this syntax is specific to this build system
-                    # we're only going to use certain compiler args with nvcc and not with gcc
-                    # the implementation of this trick is in customize_compiler() below
-                    extra_compile_args={'gcc': [],
-                                         'nvcc':compute_capability_args},
-                    include_dirs=[numpy_include, CUDA['include'], 'Source'])
+                        sources=(include_headers(['tigre/Source/POCS_TV2.cu',
+                                                     'tigre/Source/_types.pxd',
+                                                     'tigre/Source/_AwminTV.pyx'], sdist=(sys.argv[1] == "sdist"))),
+                        library_dirs=[CUDA['lib64']],
+                        libraries=['cudart'],
+                        language='c++',
+                        runtime_library_dirs=[CUDA['lib64']],
+                        # this syntax is specific to this build system
+                        # we're only going to use certain compiler args with nvcc and not with gcc
+                        # the implementation of this trick is in customize_compiler() below
+                        extra_compile_args={'gcc': [],
+                                            'nvcc': compute_capability_args},
+                        include_dirs=[numpy_include, CUDA['include'], 'Source'])
 
 
 # run the customize_compiler
@@ -195,13 +218,14 @@ class custom_build_ext(build_ext):
         build_ext.build_extensions(self)
 
 
-setup(name='tigre',
-      version = '0.1.0',
-      author = 'Reuben Lindroos, Sam loescher',
-      packages = find_packages(),
+setup(name='pytigre',
+      version='0.1.0',
+      author='Reuben Lindroos, Sam loescher',
+      packages=find_packages(),
+
       include_package_data=True,
-      ext_modules=[Ax_ext, Atb_ext,tvdenoising_ext, minTV_ext, AwminTV_ext],
-      py_modules = ['tigre.py'],
+      ext_modules=[Ax_ext, Atb_ext, tvdenoising_ext, minTV_ext, AwminTV_ext],
+      py_modules=['tigre.py'],
       # inject our custom trigger
       cmdclass={'build_ext': custom_build_ext},
 
