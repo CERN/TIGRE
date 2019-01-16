c575bfad4bf6a6bf8a81c828b2dd30722b6e7d30
reuben lindroos <reuben@dhcp-ASPIRE.soton.ac.uk>
2019-01-16 16:25:48 +0000

    removed dist.

--- Python/dist/pytigre-0.1.0/tigre/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import division, absolute_import, print_function
-from .utilities.geometry import geometry
-from .utilities.geometry_default import TIGREParameters as geometry_default
-from . import algorithms
-import tigre.demos
