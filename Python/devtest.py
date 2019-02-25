745a97988798ebef15ff2bb2347c026bd6bc5842
reuben <rllindroos@gmail.com>
2019-02-25 13:51:47 +0000

    log generator now adds git branch, repo and commit and saves files under new date

--- Python/devtest.py
+++ Python/devtest.py
@@ -10,10 +10,12 @@ nVoxel = np.array([32,64,128])
 nangles = 100
 angles_1 = np.linspace(0, 2 * np.pi, nangles, dtype=np.float32)
 angles_2 = np.zeros((nangles), dtype=np.float32) * np.array(np.pi / 4, dtype=np.float32)
-angles_3 = np.ones((nangles), dtype=np.float32)*0.0001
-angles = np.vstack((angles_1, angles_3, angles_3)).T
-geo = tigre.geometry(mode='parallel',nVoxel=nVoxel,default_geo=True)
-geo.accuracy=1.
+angles_3 = np.ones((nangles), dtype=np.float32)*np.pi
+angles = np.vstack((angles_1, angles_2, angles_2)).T
+
+geo = tigre.geometry(nVoxel=nVoxel,mode='parallel',default_geo=True)
 src_img = data_loader.load_head_phantom(nVoxel)
-proj = tigre.Ax(src_img,geo,angles)
-algs.cgls(proj,geo,angles,niter=24)
+proj=tigre.Ax(src_img,geo,angles)
+res = tigre.algorithms.fbp(proj,geo,angles,niter=10)
+plt.imshow(res[nVoxel[0]/2])
+plt.show()
--- Python/tests/testforlog.py
+++ Python/tests/testforlog.py
@@ -8,21 +8,54 @@ from matplotlib import pyplot as plt
 import time
 import tigre.algorithms as algs
 import warnings
+from matplotlib import pyplot as plt
 warnings.filterwarnings("error")
-
-def testandlog(alglist, geo, angles, niter, logfilename=None,saveresult=False):
+from git import Repo
+def testandlog(alglist, geo, angles, niter, logfilename=None, saveresult=False, newsubdirectory=True):
     nangles = angles.shape[0]
     # createlogfile
 
     dirname = os.path.dirname(__file__)
-    logfolder = os.path.join(dirname, 'logTIGRE')
-    if 'logTIGRE' not in os.listdir(dirname):
-        os.system('mkdir ' + logfolder)
 
+    # create top level folder if this does not exist
+
+    if 'logs' not in os.listdir(dirname):
+        os.system('mkdir ' + 'logs')
+    logdirectory = os.path.join(dirname, 'logs')
+    logdate = time.strftime("%a_%d_%Y")
+    os.system('mkdir ' + os.path.join(logdirectory,logdate))
+    subdirectory = os.path.join(logdirectory,logdate)
+    subdirectoryname = 'test' + str(len(os.listdir(subdirectory))-1)
+
+    if newsubdirectory:
+        subdirectoryname = 'test' +str(len(os.listdir(subdirectory)))
+
+    os.system('mkdir ' + os.path.join(subdirectory, subdirectoryname))
+    subdirectory = os.path.join(logdirectory,logdate, subdirectoryname)
+    timestamp = str(time.asctime()).replace(' ', '_')
     if logfilename is not None:
-        logf = open(os.path.join(logfolder, logfilename) + '.log', 'w')
+        logf = open(os.path.join(subdirectory, logfilename) + '.log', 'w')
     else:
-        logf = open(os.path.join(logfolder, str(geo.mode) + '_' + str(time.asctime()).replace(' ', '_') + '.log'), 'w')
+        logf = open(os.path.join(subdirectory, str(geo.mode) + '_' + timestamp + '.log'), 'w')
+
+    # GIT header for logfile
+
+
+    repo_path=os.path.join(dirname,'..','..','.git/')
+    repo = Repo(repo_path)
+    if not repo.bare:
+        print('Repo at {} successfully loaded.'.format(repo_path))
+        logf.write('------------------------------------------------\n')
+        logf.write(make_repository_str(repo))
+
+        # create list of commits then print some of them to stdout
+        commit = list(repo.iter_commits(repo.active_branch))[0]
+        logf.write(make_commit_str(commit))
+        logf.write('\n------------------------------------------------\n')
+    else:
+        print('Could not load repository at {} :('.format(repo_path))
+
+
     source_img = data_loader.load_head_phantom(number_of_voxels=geo.nVoxel)
     proj = tigre.Ax(source_img, geo, angles)
     logf.write('GEOMETRY used for instance of testandlog: \n')
@@ -32,7 +65,7 @@ def testandlog(alglist, geo, angles, niter, logfilename=None,saveresult=False):
     for alg in alglist:
         logf.write(str(alg).upper() + ' ' + time.asctime() + '\n')
         logf.write('nproj: ' + str(nangles) + ' niter: ' + str(niter) + '\n')
-        if np.sum(angles[1])!=0:
+        if np.sum(angles[:,1])!=0 or np.sum(angles[:,2])!=0:
             spherical = True
         else:
             spherical = False
@@ -44,6 +77,7 @@ def testandlog(alglist, geo, angles, niter, logfilename=None,saveresult=False):
             algpassed = True
         except Exception:
             formatedlines = traceback.format_exc()
+            logf.write('ERROR at ' + str(time.strftime("%H:%M:%S")+'\n'))
             logf.write(''.join(formatedlines) + '\n')
         finally:
             toc = time.clock()
@@ -53,29 +87,39 @@ def testandlog(alglist, geo, angles, niter, logfilename=None,saveresult=False):
         logf.write('------------------------------------------------\n')
 
         if saveresult and algpassed:
-            npsavefile = os.path.join(logfolder, 'numpysavefile')
-            if 'numpysavefile' not in os.listdir(logfolder):
-                os.system('mkdir '+npsavefile)
-            np.save(os.path.join(npsavefile,alg+'_'+os.path.splitext(os.path.split(logf.name)[-1])[0]),res)
+            warnings.filterwarnings("ignore")
+            plt.subplot(3, 1, 1)
+            plt.imshow(res[geo.nVoxel[0]/2])
+            plt.title('results for '+alg)
+            plt.ylabel('dim 0')
 
+            plt.subplot(3, 1, 2)
+            plt.imshow(res[:,geo.nVoxel[1]/2])
+            plt.xlabel('time (s)')
+            plt.ylabel('dim 1')
 
-
-alglist = ['sirt','cgls','fbp']
-#alglist.remove('sart')
-
-geo = tigre.geometry(mode='cone', nVoxel=np.array([32, 64, 128]), default_geo=True)
-niter = 10
-nangles = 100
-angles_1 = np.linspace(0, 2 * np.pi, nangles, dtype=np.float32)
-angles_2 = np.zeros((nangles), dtype=np.float32) * np.array(np.pi / 4, dtype=np.float32)
-angles_3 = np.ones((nangles), dtype=np.float32) * 0.0001
-angles = np.vstack((angles_1, angles_3, angles_3)).T
-
-
-testandlog(alglist, geo, angles,niter,saveresult=False)
-
-geo = tigre.geometry(mode='parallel', nVoxel=np.array([32, 64, 128]), default_geo=True)
-niter = 10
-nangles = 100
-
-testandlog(alglist, geo, angles,niter,saveresult=True)
+            plt.subplot(3, 1, 3)
+            plt.imshow(res[:,:,geo.nVoxel[2]/2])
+            plt.ylabel('dim 2')
+            plt.savefig(os.path.join(subdirectory, alg+ '_'+geo.mode+'_' +timestamp+'.png'))
+            warnings.filterwarnings("error")
+def make_commit_str(commit):
+    l = []
+    l.append(str(commit.hexsha))
+    l.append("\"{}\" by {} ({})".format(commit.summary,
+                                     commit.author.name,
+                                     commit.author.email))
+    l.append(str(commit.authored_datetime))
+    l.append(str("count: {} and size: {}".format(commit.count(),
+                                              commit.size)))
+    commit_str=('\n'.join(l))
+    return commit_str
+def make_repository_str(repo):
+    l = []
+    l.append('Repo description: {}'.format(repo.description))
+    l.append('Repo active branch is {}'.format(repo.active_branch))
+    for remote in repo.remotes:
+        l.append('Remote named "{}" with URL "{}"'.format(remote, remote.url))
+    l.append('Last commit for repo is {}.'.format(str(repo.head.commit.hexsha)))
+    repo_str = ('\n'.join(l))
+    return repo_str
