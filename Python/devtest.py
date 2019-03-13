fixed error with BP nangles<32. Also fixed error with filtering that was causing awasd_pocs and asd_pocs to output wrong results.

Python/devtest.py
@@ -7,35 +7,14 @@ import tigre.algorithms as algs
 from tigre.demos.Test_data import data_loader
 from matplotlib import pyplot as plt
 
-nVoxel = np.array([256,256,256])
+geo = tigre.geometry(mode='parallel', nVoxel=np.array([32, 64, 128]), default_geo=True)
+niter = 10
 nangles = 100
 angles_1 = np.linspace(0, 2 * np.pi, nangles, dtype=np.float32)
-angles_2 = np.zeros((nangles), dtype=np.float32) * np.array(np.pi / 4, dtype=np.float32)
-angles_3 = np.ones((nangles), dtype=np.float32)*np.pi
-angles = np.vstack((angles_1, angles_3, angles_3)).T
-
-geo_cone = tigre.geometry(nVoxel=nVoxel,mode='parallel',default_geo=True)
-geo_para = tigre.geometry(nVoxel=nVoxel,mode='cone',default_geo=True)
-
-head = data_loader.load_head_phantom(nVoxel)
-
-proj_cone = tigre.Ax(head,geo_cone,angles,'interpolated')
-proj_par = tigre.Ax(head,geo_para,angles)
-
-
-"""
-backproj_cone = tigre.Atb(proj_cone,geo_cone,angles,'FDK')
-backproj_par = tigre.Atb(proj_par,geo_para,angles,'FDK')
-"""
-#algossart = algs.ossart(proj_cone,geo_cone,angles,niter=10)
-
-"""
-plt.figure()
-plt.subplot(2,1,1)
-plt.imshow(backproj_cone[128])
-plt.subplot(2,1,2)
-plt.imshow(backproj_par[128])
-
+angles_2 = np.zeros((nangles), dtype=np.float32)
+angles = np.vstack((angles_1, angles_2, angles_2)).T
+head = data_loader.load_head_phantom(geo.nVoxel)
+proj = tigre.Ax(head,geo,angles)
+output = algs.awasd_pocs(proj,geo,angles,niter=10,blocksize=20)
+plt.imshow(output[geo.nVoxel[0]/2])
 plt.show()
-"""
-

Python/tigre/utilities/filtering.py
@@ -10,7 +10,7 @@ def filtering(proj,geo,angles,parker,verbose=False):
     if parker:
         proj=parkerweight(proj.transpose(0,2,1),geo,angles,parker).transpose(0,2,1)
         # proj=parkerweight(proj,geo,angles,parker)
-    filt_len=max(64,2**nextpow2(2*geo.nDetector[0]))
+    filt_len=max(64,2**nextpow2(2*max(geo.nDetector)))
     ramp_kernel=ramp_flat(filt_len)
 
     d=1
@@ -18,7 +18,6 @@ def filtering(proj,geo,angles,parker,verbose=False):
     filt=np.kron(np.ones((np.int64(geo.nDetector[0]),1)),filt)
     for i in range(angles.shape[0]):
         fproj=np.zeros((geo.nDetector[0],filt_len),dtype=np.float32)
-        y_dim = geo.nDetector[1]
         fproj[:,int(filt_len/2-geo.nDetector[1]/2):int(filt_len/2+geo.nDetector[1]/2)]=proj[i]
         fproj=np.fft.fft(fproj,axis=1)
 

Python/tigre/utilities/geometry.py
@@ -231,8 +231,11 @@ class ParallelGeo(Geometry):
         self.DSD = np.float32(self.nVoxel[0] * 2)
 
         self.dDetector = np.array([1, 1])
-        self.nDetector = self.nVoxel[:2]
-        self.sDetector = self.nVoxel[:2]
+        self.nDetector = np.zeros((2,),dtype = np.int64)
+        self.nDetector[0] = self.nVoxel[0]
+        self.nDetector[1] = max(self.nVoxel[1],self.nVoxel[2])
+        #self.nDetector = self.nVoxel[:2]
+        self.sDetector = self.nDetector
 
         self.accuracy = 0.5
 