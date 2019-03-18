27c58dd6afbcc9fcd73bc2f0aa1aeedcc17c82b8
reuben <rllindroos@gmail.com>
2019-03-18 15:31:41 +0000

    in the process of modifying iterative_reconalg s.th V and W are computed more efficiently

--- Python/tigre/utilities/Atb.py
+++ Python/tigre/utilities/Atb.py
@@ -9,6 +9,11 @@ def Atb(projections, geo, angles,  krylov="matched"):
         raise ValueError("Complex types not compatible for back projection.")
     geox = copy.deepcopy(geo)
     geox.check_geo(angles)
+    """
+       Here we cast all values in geo to single point precision float. This way we 
+       know what behaviour to expect from pytigre to Cuda and can change 
+       single parameters accordingly. 
+    """
     geox.cast_to_single()
     geox.checknans()
 
--- Python/tigre/utilities/Ax.py
+++ Python/tigre/utilities/Ax.py
@@ -10,6 +10,11 @@ def Ax(img, geo, angles,  krylov="ray-voxel"):
         raise ValueError("Complex types not compatible for projection.")
     geox = copy.deepcopy(geo)
     geox.check_geo(angles)
+    """
+    Here we cast all values in geo to single point precision float. This way we 
+    know what behaviour to expect from pytigre to Cuda and can change 
+    single parameters accordingly. 
+    """
     geox.cast_to_single()
     geox.checknans()
 
