6b8f6bd036b0ecb157dcce28ebbdb0d1405070a3
reubenlindroos <rllindroos@gmail.com>
2019-03-21 14:46:42 +0000

    further developement on ista

--- Python/goingoldschool.py
+++ Python/goingoldschool.py
@@ -4,7 +4,7 @@ import tigre
 import copy
 from _tvdenoising import tvdenoise
 from tigre.demos.Test_data import data_loader
-
+import time
 
 def fista(y, geo, angles, niter, trueimg):
     lmbda = 0.1
@@ -15,21 +15,29 @@ def fista(y, geo, angles, niter, trueimg):
     bm = 1 / L
     relativeError = []
     for i in range(niter):
+
         y_rec += bm * 2 * (tigre.Atb((y - tigre.Ax(y_rec, geo, angles, 'interpolated')),
                                      geo, angles, 'FDK'))
+        if i ==0:
+            print('FISTA algorithm in progress.')
+            toc = time.clock()
+        if i ==1:
+            tic = time.clock()
+            print('Estimated time until completion: ' +str(niter*(tic-toc))+ '(s)')
+
+        lambdaForTv = 2 * bm * lmbda
+        find_nan(y_rec,22,i)
+        #y_rec = tvdenoise(y_rec, 20, 1./lambdaForTv)
+        #find_nan(y_rec,22,i)
 
 
-        #lambdaForTv = 2 * bm * lmbda
-        find_nan(y_rec,22,i)
-        y_rec = tvdenoise(y_rec, 20, 12.0)
-        find_nan(y_rec,22,i)
-        """
+
         x_rec_old = copy.deepcopy(x_rec)
-        x_rec = tvdenoise(y_rec, 0, lambdaForTv)
+        x_rec = tvdenoise(y_rec, 20, 1./lambdaForTv)
         t_old = t
         t = (1 + np.sqrt(1 + 4 * t ** 2)) / 2
         y_rec = x_rec + (t_old - 1) / t * (x_rec - x_rec_old)
-        """
+
         relativeError.append(np.linalg.norm((y_rec-trueimg).ravel(),2)/np.linalg.norm(trueimg.ravel(),2))
 
     return y_rec ,relativeError
@@ -48,7 +56,7 @@ geo = tigre.geometry(mode='cone', nVoxel=np.array([64, 64, 64]), default_geo=Tru
 
 src_img = data_loader.load_head_phantom(geo.nVoxel)
 proj = tigre.Ax(src_img,geo,angles)
-output, relativeError = fista(proj,geo,angles,50,src_img)
+output, relativeError = fista(proj,geo,angles,200,src_img)
 from matplotlib import pyplot as plt
 plt.plot(relativeError)
 plt.show()
