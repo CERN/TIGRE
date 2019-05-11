4d6b5f248a66473ea843d2bc94dc46f55cb5f2ea
kaushl2208 <kaushlendrapratap.9837@gmail.com>
2019-05-11 23:58:41 +0530

    Tests made PEP8 compliant

--- Python/tests/generate_configurations.py
+++ Python/tests/generate_configurations.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tigre
 import os
-nVoxel = np.array([256,256,256])
+nVoxel = np.array([256, 256, 256])
 niter = 20
 dirname = os.path.dirname(__file__)
 keywords = dict(blocksize=20)
@@ -11,19 +11,21 @@ def save_config(dict, name):
     for kw in dict:
         if kw not in ['nproj', 'geo', 'angles', 'niter', 'kwargs']:
             raise KeyError()
-    np.save(os.path.join(dirname, name),dict)
+    np.save(os.path.join(dirname, name), dict)
 
 
 if __name__ == '__main__':
 
-    geo = tigre.geometry(mode='cone',nVoxel=nVoxel,default=True)
-    geo.nDetector = np.array((256, 256))                        # (V,U) number of pixels        (px)
-    geo.dDetector = np.array((0.8, 0.8)) * 2                    # size of each pixel            (mm)
+    geo = tigre.geometry(mode='cone', nVoxel=nVoxel, default=True)
+    # (V,U) number of pixels        (px)
+    geo.nDetector = np.array((256, 256))
+    # size of each pixel            (mm)
+    geo.dDetector = np.array((0.8, 0.8)) * 2
     geo.sDetector = geo.nDetector * geo.dDetector
 
     save_config(dict(
         nproj=100,
-        niter = 20,
+        niter=20,
         geo=geo,
         angles=np.linspace(0, 2 * np.pi, 100),
         kwargs=keywords
@@ -32,13 +34,14 @@ if __name__ == '__main__':
 
     save_config(dict(
         nproj=100,
-        niter = 20,
-        geo=tigre.geometry(mode='parallel', default=True, nVoxel=np.array([256,256,256])),
+        niter=20,
+        geo=tigre.geometry(mode='parallel', default=True, nVoxel=np.array([256, 256, 256])),
         angles=np.linspace(0, 2 * np.pi, 100),
         kwargs=keywords
     ),
         'configuration2.npy')
-    geo = tigre.geometry(mode='cone', nVoxel=np.array([240,248,256]), default=True)
+    geo = tigre.geometry(mode='cone', nVoxel=np.array(
+        [240, 248, 256]), default=True)
     geo.nDetector = np.array((248, 256))
     geo.dDetector = np.array((0.8, 0.8)) * 2
     geo.sDetector = geo.nDetector * geo.dDetector
