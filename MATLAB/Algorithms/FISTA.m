49fb22ab1a0055d0c29afd03bc62bb8ebf605ff7
reuben <rllindroos@gmail.com>
2019-07-24 15:13:43 +0100

    Fixes #134.

--- MATLAB/Algorithms/FISTA.m
+++ MATLAB/Algorithms/FISTA.m
@@ -1,10 +1,17 @@
-function [res] = FISTA(proj,geo,angles,niter,hyper)
-% FISTA is a quadratically converging algorithm that relies on the hyper
-% parameter named 'hyper'. This parameter should approximate the largest 
-% eigenvalue in the A matrix in the equation Ax-b and Atb. Empirical tests
-% show that for, the headphantom object:
-%           geo.nVoxel = [64,64,64]'    ,      hyper (approx=) 2.e8
-%           geo.nVoxel = [512,512,512]' ,      hyper (approx=) 2.e4
+function [res] = FISTA(proj,geo,angles,niter,varargin)
+% FISTA is a quadratically converging algorithm.
+
+% 'hyper': This parameter should approximate the largest 
+%          eigenvalue in the A matrix in the equation Ax-b and Atb. 
+%          Empirical tests show that for, the headphantom object:
+%               
+%               geo.nVoxel = [64,64,64]'    ,      hyper (approx=) 2.e8
+%               geo.nVoxel = [512,512,512]' ,      hyper (approx=) 2.e4
+%          Default: 2.e8
+% 'tviter':  Number of iterations of Im3ddenoise to use. Default: 20
+% 'lambda':  Multiplier for the tvlambda used, which is proportional to 
+%            L (hyper). Default: 0.1
+% 'verbose': get feedback or not. Default: 1
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
 % This file is part of the TIGRE Toolbox
@@ -21,8 +28,7 @@ function [res] = FISTA(proj,geo,angles,niter,hyper)
 % Codes:              https://github.com/CERN/TIGRE/
 % Coded by:           Ander Biguri, Reuben Lindroos
 %--------------------------------------------------------------------------
-
-lambda = 0.1;
+[verbose,tviter,hyper,lambda]=parse_inputs(proj,geo,angles,varargin);
 res = zeros(geo.nVoxel','single');
 x_rec = res;
 L = hyper;
@@ -34,11 +40,11 @@ for ii = 1:niter
     res = res + bm * 2 * Atb(proj - Ax(res,geo,angles, 'ray-voxel'), geo, angles, 'matched');
     lambdaforTV = 2* bm* lambda;
     x_recold = x_rec;
-    x_rec = im3DDenoise(res,'TV',20,1/lambdaforTV);  
+    x_rec = im3DDenoise(res,'TV',tviter,1/lambdaforTV);  
     told = t;
     t = ( 1+sqrt(1+4*t*t) ) / 2;
     res= x_rec + (told-1)/t * (x_rec - x_recold);
-    if (ii==1);
+    if (ii==1)&&(verbose==1);
         expected_time=toc*niter;
         disp('FISTA');
         disp(['Expected duration  :    ',secs2hms(expected_time)]);
@@ -48,4 +54,83 @@ for ii = 1:niter
     
 end
 
+end
+%% Parse inputs
+function [verbose,tviter,hyper,lambda]=parse_inputs(proj,geo,angles,argin)
+opts=     {'lambda','tviter','verbose','hyper'};
+defaults=ones(length(opts),1);
+% Check inputs
+nVarargs = length(argin);
+if mod(nVarargs,2)
+    error('TIGRE:FISTA:InvalidInput','Invalid number of inputs')
+end
+
+% check if option has been passed as input
+for ii=1:2:nVarargs
+    ind=find(ismember(opts,lower(argin{ii})));
+    if ~isempty(ind)
+        defaults(ind)=0;
+    else
+        error('TIGRE:FISTA:InvalidInput',['Optional parameter "' argin{ii} '" does not exist' ]);
+    end
+end
+
+for ii=1:length(opts)
+    opt=opts{ii};
+    default=defaults(ii);
+    % if one option isnot default, then extranc value from input
+    if default==0
+        ind=double.empty(0,1);jj=1;
+        while isempty(ind)
+            ind=find(isequal(opt,lower(argin{jj})));
+            jj=jj+1;
+        end
+        if isempty(ind)
+            error('TIGRE:FISTA:InvalidInput',['Optional parameter "' argin{jj} '" does not exist' ]);
+        end
+        val=argin{jj};
+    end
+    
+    switch opt
+        % % % % % % % Verbose
+        case 'verbose'
+            if default
+                verbose=1;
+            else
+                verbose=val;
+            end
+            if ~is2014bOrNewer
+                warning('TIGRE: Verbose mode not available for older versions than MATLAB R2014b');
+                verbose=false;
+            end
+            % % % % % % % hyperparameter, LAMBDA
+        case 'lambda'
+            if default
+                lambda=0.1;
+            elseif length(val)>1 || ~isnumeric( val)
+                error('TIGRE:OS_SART:InvalidInput','Invalid lambda')
+            else
+                lambda=val;
+            end
+        case 'hyper'
+            if default
+                hyper = 2.e8;
+            elseif length(val)>1 || ~isnumeric( val)
+                error('TIGRE:OS_SART:InvalidInput','Invalid lambda')
+            else 
+                hyper = val;
+            end
+        case 'tviter'
+            if default
+                tviter = 20;
+            elseif length(val)>1 || ~isnumeric( val)
+                error('TIGRE:OS_SART:InvalidInput','Invalid lambda')
+            else
+                tviter = val;
+            end
+               
+        otherwise
+            error('TIGRE:OS_SART:InvalidInput',['Invalid input name:', num2str(opt),'\n No such option in OS_SART_CBCT()']);
+    end
+end
 end
---
+++ MATLAB/Demos/d20_AlgorithmsFISTA.m
@@ -0,0 +1,82 @@
+%% DEMO 9:  Algorithms 04. Total Variation minimization algorithms
+%
+%
+%  This demo presents the Total variation algorithms in TIGRE. Total
+%  variation algorithms try to minimize the variation (gradient) of the
+%  image, assuming its piecewise smooth, as most things in nature are (i.e.
+%  human body). 
+%
+% This set of algorithms is specially good performing when the noise is
+% very big or the number of projections is small, however, they require more
+% computational time and memory than the other algorithms to run.
+% 
+% 
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+% This file is part of the TIGRE Toolbox
+% 
+% Copyright (c) 2015, University of Bath and 
+%                     CERN-European Organization for Nuclear Research
+%                     All rights reserved.
+%
+% License:            Open Source under BSD. 
+%                     See the full license at
+%                     https://github.com/CERN/TIGRE/blob/master/LICENSE
+%
+% Contact:            tigre.toolbox@gmail.com
+% Codes:              https://github.com/CERN/TIGRE/
+% Coded by:           Ander Biguri 
+%--------------------------------------------------------------------------
+%% Initialize
+clear;
+close all;
+
+%% Define Geometry
+geo=defaultGeometry('nVoxel',[128;128;128]);                     
+
+%% Load data and generate projections 
+% see previous demo for explanation
+angles=linspace(0,2*pi-2*pi/30,30);
+head=headPhantom(geo.nVoxel);
+projections=Ax(head,geo,angles,'interpolated');
+noise_projections=addCTnoise(projections);
+
+%% FISTA is a quadratically converging algorithm.
+
+% 'hyper': This parameter should approximate the largest 
+%          eigenvalue in the A matrix in the equation Ax-b and Atb. 
+%          Empirical tests show that for, the headphantom object:
+%               
+%               geo.nVoxel = [64,64,64]'    ,      hyper (approx=) 2.e8
+%               geo.nVoxel = [512,512,512]' ,      hyper (approx=) 2.e4
+%          Default: 2.e8
+% 'tviter':  Number of iterations of Im3ddenoise to use. Default: 20
+% 'lambda':  Multiplier for the tvlambda used, which is proportional to 
+%            L (hyper). Default: 0.1
+% 'verbose': get feedback or not. Default: 1
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+% This file is part of the TIGRE Toolbox
+%
+% Copyright (c) 2015, University of Bath and
+%                     CERN-European Organization for Nuclear Research
+%                     All rights reserved.
+%
+% License:            Open Source under BSD.
+%                     See the full license at
+%                     https://github.com/CERN/TIGRE/blob/master/LICENSE
+%
+% Contact:            tigre.toolbox@gmail.com
+% Codes:              https://github.com/CERN/TIGRE/
+% Coded by:           Ander Biguri, Reuben Lindroos
+%--------------------------------------------------------------------------
+imgFISTA_default = FISTA(noise_projections,geo,angles,100);
+
+%% Adding a different hyper parameter
+imgFISTA_hyper = FISTA(noise_projections,geo,angles,100,'hyper' ,2.e6);
+
+%% Recon more significant tv parameters
+imgFISTA_hightv = FISTA(noise_projections,geo,angles,100,'hyper',2.e6,'tviter',100,'lambda',20);
+
+%% Plot results
+plotImg([imgFISTA_default,imgFISTA_hyper,imgFISTA_hightv])
