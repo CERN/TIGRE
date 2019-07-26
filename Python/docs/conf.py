8ff560d4589e173840df2b81aee2df37aabd52af
reuben <rllindroos@gmail.com>
2019-07-26 14:27:59 +0100

    updated conf.py

--- Python/docs/conf.py
+++ Python/docs/conf.py
@@ -50,7 +50,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     #'nbsphinx'
-    'IPython.sphinxext.ipython_console_highlighting'
+    'sphinxext.ipython_console_highlighting'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
