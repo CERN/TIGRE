de20a549347b9757a0b1ae11c493d0b6a268a573
reuben <rllindroos@gmail.com>
2019-08-03 16:17:11 +0100

    Fixes #138

--- Python/tigre/utilities/Ax.py
+++ Python/tigre/utilities/Ax.py
@@ -9,7 +9,6 @@ def Ax(img, geo, angles,  krylov="ray-voxel"):
     if not np.isreal(img).all():
         raise ValueError("Complex types not compatible for projection.")
     geox = copy.deepcopy(geo)
-    geox.check_geo(angles)
     """
     Here we cast all values in geo to single point precision float. This way we 
     know what behaviour to expect from pytigre to Cuda and can change 
