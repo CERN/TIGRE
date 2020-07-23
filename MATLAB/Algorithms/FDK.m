4e57c3806a313405e8b62de352dfc64224b8ddc6
Brandon Nelson <bnel1201@gmail.com>
2020-07-22 19:32:54 -0500

    Added checks before applying Wang pre-weighting
    Only off-detector scans with nonzero off-detector position and fixed
    geometry (constant detector position and constant DSO/DSD) will have
    Wang pre-weighting applied.

--- MATLAB/Algorithms/FDK.m
+++ MATLAB/Algorithms/FDK.m
@@ -18,12 +18,12 @@ function [res]=FDK(proj,geo,angles,varargin)
 %
 % Contact:            tigre.toolbox@gmail.com
 % Codes:              https://github.com/CERN/TIGRE/
-% Coded by:           Kyungsang Kim, modified by Ander Biguri 
+% Coded by:           Kyungsang Kim, modified by Ander Biguri, Brandon Nelson 
 %--------------------------------------------------------------------------
 [filter,parker]=parse_inputs(proj,geo,angles,varargin);
 
-if abs(geo.offDetector(1)) > 0
-    proj = apply_wang_weights(proj, geo);
+if apply_wang_weights(geo)
+    proj = wang_displaced_detector_weighting(proj, geo);
 end
 
 geo=checkGeo(geo,angles);
@@ -123,13 +123,12 @@ end
 
 end
 
-function proj = apply_wang_weights(proj, geo)
-
+function proj = wang_displaced_detector_weighting(proj, geo)
     overlap_in_mm = geo.sDetector(1)/2 - abs(geo.offDetector(1));
     overlap_in_pix = round(overlap_in_mm / geo.dDetector(1));
 
     t_in_mm = linspace(-overlap_in_mm, overlap_in_mm, 2 * overlap_in_pix);
-    R_in_mm = geo.DSO;
+    R_in_mm = geo.DSO(1);
     w = 0.5 * (sin((pi * atan(t_in_mm / R_in_mm)) / (2 * atan(overlap_in_mm / R_in_mm))) + 1);
     w = repmat(w, [geo.nDetector(1), 1, size(proj, 3)]);
 
@@ -140,4 +139,28 @@ function proj = apply_wang_weights(proj, geo)
     end
     wang_weights = 2 * wang_weights;
     proj = proj .* wang_weights;
+end
+
+function bool = apply_wang_weights(geo)
+    if size(geo.offDetector,2) > 1
+        bool = false;
+        return
+    end
+    
+    if geo.offDetector(1) == 0
+        bool = false;
+        return
+    end
+    
+    if numel(geo.DSO) > 1
+        bool = false;
+        return
+    end
+
+    percent_offset = abs(geo.offDetector(1)/geo.sDetector(1)) * 100;    
+    if percent_offset > 30
+        warning("Detector offset percent: %0.2f) is greater than 30 which may result in image artifacts, consider rebinning 360 degree projections to 180 degrees", percent_offset)
+    end
+    
+    bool = true;
 end
