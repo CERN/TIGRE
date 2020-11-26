df7a0c418338ca180fac9dd462311d19558aba82
Ander Biguri <ander.biguri@gmail.com>
2020-11-26 19:16:50 +0000

    Geometry defaults for varian

--- MATLAB/Utilities/IO/VarianCBCT/GeometryFromXML.m
+++ MATLAB/Utilities/IO/VarianCBCT/GeometryFromXML.m
@@ -1,7 +1,11 @@
-function [geo, ScanXML, ReconXML] = GeometryFromXML(datafolder)
+function [geo, ScanXML, ReconXML] = GeometryFromXML(datafolder,varargin)
 % Dependence: xml2struct.m
 % Date: 2020-03-28
-
+if nargin>1
+    load_geo=varargin{1}~=0;
+else
+    load_geo=true;
+end
 %% Scan.xml
 filestr = dir([datafolder filesep 'Scan.xml']);
 
@@ -10,10 +14,12 @@ ScanXML = getfield(xml2struct(fullfile(filestr.folder, filestr.name)),...
 
 %% Reconstruction.xml
 filestr = dir([datafolder filesep '**' filesep 'Reconstruction.xml']);
-
-ReconXML = getfield(xml2struct(fullfile(filestr.folder, filestr.name)),...
-    'Reconstruction');
-
+ReconXML=[];
+if ~isempty(filestr) && load_geo
+    warning('Loading up geometry from already reconstructed dataset. Call geo=GeometryFromXML(datafolder,false) to autocompute plausible geometry')
+    ReconXML = getfield(xml2struct(fullfile(filestr.folder, filestr.name)),...
+        'Reconstruction');
+end
 %% Generate Scan Parameters
 % Cone-beam CT scenario
 geo.mode = 'cone';
@@ -63,15 +69,21 @@ geo.accuracy=0.5;
 
 %% Generate Image Parameters
 % total size of the image       (mm)
-geo.sVoxel=[str2double(ReconXML.VOISizeX.Text);...
-    str2double(ReconXML.VOISizeY.Text);...
-    str2double(ReconXML.VOISizeZ.Text)];
-% number of voxels              (vx)
-SliceNO = round(geo.sVoxel(3)/str2double(ReconXML.SliceThickness.Text));
-geo.nVoxel=[str2double(ReconXML.MatrixSize.Text);
-    str2double(ReconXML.MatrixSize.Text);
-    SliceNO];
-% size of each voxel            (mm)
-geo.dVoxel=geo.sVoxel./geo.nVoxel;
+if ~isempty(ReconXML) %% if we are copying some already reconstructed image, then take the params
+    geo.sVoxel=[str2double(ReconXML.VOISizeX.Text);...
+        str2double(ReconXML.VOISizeY.Text);...
+        str2double(ReconXML.VOISizeZ.Text)];
+    % number of voxels              (vx)
+    SliceNO = round(geo.sVoxel(3)/str2double(ReconXML.SliceThickness.Text));
+    geo.nVoxel=[str2double(ReconXML.MatrixSize.Text);
+        str2double(ReconXML.MatrixSize.Text);
+        SliceNO];
+    % size of each voxel            (mm)
+    geo.dVoxel=geo.sVoxel./geo.nVoxel;
+else
+    geo.dVoxel=[geo.dDetector(1);geo.dDetector(1); geo.dDetector(2)]*geo.DSO/geo.DSD;
+    geo.nVoxel=ceil([geo.nDetector(1)+abs(geo.offDetector(1))/geo.dDetector(1);geo.nDetector(1)+abs(geo.offDetector(1))/geo.dDetector(1);geo.nDetector(2)]);
+    geo.sVoxel=geo.nVoxel.*geo.dVoxel;
+end
 
 end
