b33bd91a599867aa6b7f60a8b72f682fd7049715
Tomo <genusn@gmail.com>
2020-12-01 14:44:03 +0900

    Added and modified files to boost development.

--- Python/CopyAndRun.bat
+++ Python/CopyAndRun.bat
@@ -1,11 +1,3 @@
-xcopy build\lib.win-amd64-3.7\_Atb.cp37-win_amd64.pyd         .\ /y
-xcopy build\lib.win-amd64-3.7\_AwminTV.cp37-win_amd64.pyd     .\ /y
-xcopy build\lib.win-amd64-3.7\_Ax.cp37-win_amd64.pyd          .\ /y
-xcopy build\lib.win-amd64-3.7\_gpuUtils.cp37-win_amd64.pyd    .\ /y
-xcopy build\lib.win-amd64-3.7\_minTV.cp37-win_amd64.pyd       .\ /y
-xcopy build\lib.win-amd64-3.7\_tvdenoising.cp37-win_amd64.pyd .\ /y
-rem xcopy build\lib.win-amd64-3.7\_gpuUtils.cp37-win_amd64.pyd    .\ /y
-
 Python tests\list_gpu.py
 REM pause
 python example.py
---
+++ Python/MakeLibs.bat
@@ -0,0 +1,1 @@
+Python setup.py develop --user > Build.log 2>&1
