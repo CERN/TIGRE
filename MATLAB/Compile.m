adb7b9376b7ec11b6034e732c7338aa91de4ef5c
Ander Biguri <ander.biguri@gmail.com>
2021-02-01 11:14:24 +0100

    Add code to look for CUDA in matlab
    same logic as setup.py

--- MATLAB/Compile.m
+++ MATLAB/Compile.m
@@ -28,17 +28,16 @@ clear all;
 
 mex -setup
 
-
-
 if ispc
     currentFolder = cd;
     fileExisting  = (exist(fullfile(currentFolder, 'mex_CUDA_win64.xml'), 'file') == 2);
     if ~fileExisting
         error(sprintf('mex_CUDA_win64.xml not found. You may need to rename the existing files depending on your MVS version')) ;
     end
-    cudapath=getenv('CUDA_PATH');
+%     cudapath=getenv('CUDA_PATH');
+    [cudapath, cuda_ver]=locate_cuda();
     if isempty(cudapath)
-        error(sprintf('CUDA Path not found. \nAdd the path by writting in MATLAB:\nsetenv(''CUDA_PATH'',''your path'')\nWhere "your path" is C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v8.0, for example')) ;
+        error(sprintf('CUDA Path not found. \nAdd the path by writting in MATLAB:\nsetenv(''CUDA_PATH'',''your path'')\nWhere "your path" is C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2, for example, \nor /usr/local/cuda on linux')) ;
     end
 end
 
@@ -47,7 +46,7 @@ disp('Compiling TIGRE source...')
 disp('This may take a couple of minutes....')
 
 if ispc
-
+    
     if ~isempty(strfind(computer('arch'),'64'))
         mex -largeArrayDims ./Source/Ax_mex.cpp ./Source/ray_interpolated_projection.cu ./Source/Siddon_projection.cu ./Source/ray_interpolated_projection_parallel.cu ./Source/Siddon_projection_parallel.cu ./Source/GpuIds.cpp -outdir ./Mex_files/win64
         mex -largeArrayDims ./Source/Atb_mex.cpp ./Source/voxel_backprojection.cu ./Source/voxel_backprojection2.cu  ./Source/voxel_backprojection_parallel.cu ./Source/GpuIds.cpp -outdir ./Mex_files/win64
@@ -112,9 +111,72 @@ elseif isunix
     end
 end
 
-
-
-
-
 disp('')
 disp('Compilation complete')
+
+
+function [cuda_path, cuda_ver]=locate_cuda()
+
+cuda_ver=-1;
+% Guess 1:
+cuda_path=getenv('CUDA_PATH');
+if isempty(cuda_path)
+    cuda_path=getenv('CUDA_HOME');
+end
+if ~isempty(cuda_path) % we have something.
+    cuda_ver=get_cuda_ver(cuda_path);
+    return
+end
+% Guess 2:
+if ispc
+    which='where';
+else
+    which='which';
+end
+[status,cmout]=system([which, ' nvcc']);
+if ~status % succeded
+    verstr=strsplit(cmout,'\n');
+    %which one to use? the first one I guess.
+    verstr=verstr{1};
+    cuda_path=strsplit(verstr,'\\bin');
+    cuda_path=cuda_path{1};
+    cuda_ver=get_cuda_ver(cuda_path);
+    return
+end
+% Guess 3
+if ispc
+    guess_cuda_path='C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/';
+    if exist(guess_cuda_path, 'dir')
+        versions=ls(guess_cuda_path);
+        % just grab biggest one.
+        versions_num=str2double(versions(3:end,2:end));
+        [~,idx]=max(versions_num);
+        cuda_path=[guess_cuda_path,versions(2+idx,:)];
+        cuda_ver=get_cuda_ver(cuda_path);
+        return
+    end
+else
+    % symlinc
+    guess_cuda_path='/usr/local/cuda';
+    if exist(guess_cuda_path, 'dir')
+        cuda_path=guess_cuda_path;
+        cuda_ver=get_cuda_ver(cuda_path);
+        return
+    end
+end
+
+
+end
+function cuda_ver=get_cuda_ver(cuda_path)
+    if ispc
+        [status,cmout]=system(['"', cuda_path, '/bin/nvcc" -V']);
+    else
+        [status,cmout]=system([cuda_path, '/bin/nvcc -V']);
+    end
+    if status
+        error('Error finding CUDA version')
+    else
+        stridx=strfind(cmout,'release ');
+        cuda_ver=str2double(cmout(stridx+length('release ') : stridx+length('release ')+3));
+    end
+end
