db47f62bd2deb17379a18c843b582d17fdb31427
Ander Biguri <ander.biguri@gmail.com>
2021-04-14 16:18:50 +0100

    resolve issue with deleted init

---
+++ Python/tigre/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import division, absolute_import, print_function
+from .utilities.geometry import geometry
+from .utilities.geometry_default import ConeGeometryDefault as geometry_default
+from .utilities.Ax import Ax
+from .utilities.Atb import Atb
+from .utilities.plotproj import plotproj, plotProj, plotSinogram
+from .utilities.plotimg import plotimg, plotImg
+from .utilities.plot_geometry import plot_geometry
+from . import algorithms
+import tigre.demos
