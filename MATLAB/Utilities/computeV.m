3914834b9cd031075bafcc1de2946eee8c4ef0b2
Ander Biguri <ander.biguri@gmail.com>
2021-05-22 11:32:16 +0100

    Fix #301
    Wang weigths had an extra x2 and changed Compute V to just eps the division.

--- MATLAB/Utilities/computeV.m
+++ MATLAB/Utilities/computeV.m
@@ -8,9 +8,10 @@ if ~isfield(geo,'mode')||~strcmp(geo.mode,'parallel')
         auxang=alphablocks{ii};
         auxindex=orig_index{ii};
         auxgeo = geo;
-        % shrink the volume to avoiding zeros in backprojection
-        auxgeo.sVoxel = auxgeo.sVoxel * max(auxgeo.sVoxel(1:2)/norm(auxgeo.sVoxel(1:2),2)) * 0.9;
-        auxgeo.dVoxel = auxgeo.sVoxel ./ auxgeo.nVoxel;
+        % expand the detector to avoiding zeros in backprojection
+        maxsize=max(auxgeo.sVoxel+geo.offOrigin(:,auxindex),[],2);
+%         auxgeo.sDetector=max(auxgeo.sDetector , [maxsize(1); maxsize(3)] *geo.DSD/geo.DSO);
+%         auxgeo.dDetector = auxgeo.sDetector ./ auxgeo.nDetector;
         % subset of projection angles
         auxgeo.DSD = geo.DSD(auxindex);
         auxgeo.DSO = geo.DSO(auxindex);
@@ -18,8 +19,8 @@ if ~isfield(geo,'mode')||~strcmp(geo.mode,'parallel')
         auxgeo.offDetector = geo.offDetector(:,auxindex);
         auxgeo.rotDetector = geo.rotDetector(:,auxindex);
         auxgeo.COR = geo.COR(auxindex);
-        
-        V(:,:,ii) = mean(Atb(ones(geo.nDetector(2),geo.nDetector(1),size(auxang,2),'single'),auxgeo,auxang, 'gpuids', gpuids),3);
+        %auxgeo=geo;
+        V(:,:,ii) = mean(Atb(ones(geo.nDetector(2),geo.nDetector(1),size(auxang,2),'single'),auxgeo,auxang, 'gpuids', gpuids),3)+0.000001;
     end
 else
     for ii=1:length(alphablocks)
