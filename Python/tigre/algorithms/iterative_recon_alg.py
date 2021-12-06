Fix a problem of encountering 0 in V calculation
When using SART based algorithms with unusual geometries (for example, tomosynthesis), V could have 0.0 and which causes errors when inverting. This should fix it.

Python/tigre/algorithms/iterative_recon_alg.py
@@ -217,7 +217,7 @@ class IterativeReconAlg(object):
                 
             else:
                 V[i] *= len(self.angleblocks[i])
-                
+        V[V==0.0] = np.inf        
         
         setattr(self, 'V', V)
 