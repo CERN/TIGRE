f2005b66e25c92180c94f3d6b601d7eccddb69ca
yliu88au <75292881+yliu88au@users.noreply.github.com>
2022-02-28 21:22:12 +1100

    Create common_geometry.py
    Can rotate detector and source to an angle, and simplify staticDetLinearSourceGeo()

--- Python/tigre/utilities/common_geometry.py
+++ Python/tigre/utilities/common_geometry.py
@@ -1,37 +1,32 @@
 import numpy as np
 
 # Tomosymthesis
-def staticDetectorGeo(geo,angles):
+def staticDetectorGeo(geo,angles,rot=0):
+    # angles: angle off the aixs between the source and detector centre (on x-y plane)
+    #         when angles=0, the source is perpendicular to the detector
+    # rot: rotation of both source and detector around origin
     R = geo.DSD-geo.DSO
+    rot = rot/180*np.pi
     geo.DSD = geo.DSO + R*np.cos(angles)
     geo.offDetector = np.vstack([0*angles, R*np.sin(angles)]).T
     geo.rotDetector = np.vstack([0*angles, 0*angles, -angles]).T
+    angles += rot
     geo.angles = np.vstack([angles, 0*angles, 0*angles]).T
     
     return geo
 
 # Linear scan of source
-def staticDetLinearSourceGeo(geo,s_pos,s_rot=0,d_loc='x',d_rot=0):
+def staticDetLinearSourceGeo(geo,s_pos,s_rot=0,rot=0):
     # s_pos: distance along source scanning linear trajectry 
     #        when s_pos = 0, source is aligned to the origin and detector centre
-    # s_rot: rotation angle of the source linear trajectry on (x-y) plane 
-    # d_loc: the source location on (x-y) plane axis (when s_pos=0) 
-    # d_rot: detector rotation angle on x-y plane
-    if isinstance(d_loc, str):
-        if d_loc not in ['x','X','y','Y','+x','+X','+y','+Y','-x','-X','-y','-Y']: 
-            raise Exception('Wrong d_loc option, valid choices are {"x" "y", "-x", "-y"}')
+    # s_rot: rotation angle between the source linear trajectry and detector on (x-y) plane 
+    # rot: source and detector rotation angle around the origin (on x-y plane)
     if np.isscalar(s_rot):
         s_rot = s_rot * np.ones_like(s_pos)
-    if (abs(s_rot)>90).any():       
-        raise Exception('Source scanning rotation angle should be within (-90, 90) degree')
-    else: 
-        s_rot = s_rot/180*np.pi 
-    if np.isscalar(d_rot):
-        d_rot = d_rot * np.ones_like(s_pos)
-    if (abs(d_rot)>90).any():       
-        raise Exception('Detector rotation angle should be within (-90, 90) degree')
-    else: 
-        d_rot = d_rot/180*np.pi 
+    s_rot = s_rot / 180 *np.pi
+    if np.isscalar(rot):
+        rot = rot * np.ones_like(s_pos)
+    rot = rot/180*np.pi 
                
     ang  = np.arctan2(s_pos*np.cos(s_rot), geo.DSO + s_pos*np.sin(s_rot))
     R = geo.DSD - geo.DSO
@@ -39,13 +34,6 @@ def staticDetLinearSourceGeo(geo,s_pos,s_rot=0,d_loc='x',d_rot=0):
     geo.rotDetector = np.array([0*ang, 0*ang, -ang]).T    
     geo.DSO = np.sqrt((s_pos*np.cos(s_rot))**2 + (geo.DSO + s_pos*np.sin(s_rot))**2)
     geo.DSD = geo.DSO + R*np.cos(ang)
-    for i in range(len(s_pos)):
-        if d_loc[i] in ['y','Y','+y','+Y']:
-            ang[i] += np.pi/2 + d_rot[i]
-        elif d_loc[i] in ['-y','-Y']: 
-            ang[i] -= np.pi/2 + d_rot[i]
-        elif d_loc[i] in ['-x','-X']:
-            ang[i] += np.pi + d_rot[i]         
+    ang += rot        
     geo.angles = np.vstack([ang, 0*ang, 0*ang]).T
     return geo, ang
-
