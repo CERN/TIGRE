4baf9b47b46dcf2f08790da97d39c57abdccbddc
Ander Biguri <ander.biguri@gmail.com>
2022-09-29 18:11:00 +0100

    tessorize IRN_TV_CGLS for speed and memory

--- MATLAB/Algorithms/IRN_TV_CGLS.m
+++ MATLAB/Algorithms/IRN_TV_CGLS.m
@@ -1,15 +1,15 @@
-function [x_out,errorL2,qualMeasOut]= IRN_TV_CGLS(proj,geo,angles,niter,varargin)
-% CGLS solves the CBCT problem using the conjugate gradient least
-% squares
+function [x_out,resL2,qualMeasOut]= IRN_TV_CGLS(proj,geo,angles,niter,varargin)
+% IRN_TV_CGLS solves the IRN_TV_CGLS problem using the conjugate gradient least
+% squares with Total Variation regularization, using an inner outer scheme
 % 
-%  CGLS(PROJ,GEO,ANGLES,NITER) solves the reconstruction problem
+%  IRN_TV_CGLS(PROJ,GEO,ANGLES,NITER) solves the reconstruction problem
 %   using the projection data PROJ taken over ANGLES angles, corresponding
 %   to the geometry descrived in GEO, using NITER iterations.
 % 
-%  CGLS(PROJ,GEO,ANGLES,NITER,OPT,VAL,...) uses options and values for solving. The
+%  IRN_TV_CGLS(PROJ,GEO,ANGLES,NITER,OPT,VAL,...) uses options and values for solving. The
 %   possible options in OPT are:
 % 
-% 
+%  'lambda'  Value of regularization parameter lambda, default 10. 
 %  'Init'    Describes diferent initialization techniques.
 %             * 'none'     : Initializes the image to zeros (default)
 %             * 'FDK'      : intializes image to FDK reconstrucition
@@ -34,7 +34,7 @@ function [x_out,errorL2,qualMeasOut]= IRN_TV_CGLS(proj,geo,angles,niter,varargin
 %
 % Contact:            tigre.toolbox@gmail.com
 % Codes:              https://github.com/CERN/TIGRE/
-% Coded by:           Ander Biguri 
+% Coded by:           Malena Sabate Landman, Ander Biguri 
 %--------------------------------------------------------------------------
 
 %%
@@ -42,36 +42,15 @@ function [x_out,errorL2,qualMeasOut]= IRN_TV_CGLS(proj,geo,angles,niter,varargin
 % % An Iteratively Reweighted Norm Algorithm for Total Variation Regularization
 % % Paul Rodriguez and Brendt Wohlberg
 % % 10.1109/ACSSC.2006.354879
-% 
-% errorL2 = [];
-% for i = 1:10 % Needs to change
-% % Update weight
-% Build L = W * D as an efficient handle. Think about this.
-%
-% % Call CGLS (Maybe try with LSQR just for fun)
-% [x ,errorL2_i,qualMeasOut] = CGLS(proj,geo,angles,niter,varargin);
-%
-% errorL2 = [errorL2, errorL2_i];
-% % Malena: not sure what qualMeasOut is, so I do not know if it should be stored or
-% % updated after every inner cycle
-% end
 
-% % Start coding with an L just to test dimensions
-%
-% sizeD3_1 =    (geo.nVoxel(1)-1)*geo.nVoxel(1)*geo.nVoxel(1) ...
-%             + geo.nVoxel(1)*(geo.nVoxel(1)-1)*geo.nVoxel(1) ...
-%             + geo.nVoxel(1)*geo.nVoxel(1)*(geo.nVoxel(1)-1);
-% sizeD3_2 =  geo.nVoxel(1)*geo.nVoxel(1)*geo.nVoxel(1);
-
-lambda = 10; %% ||Ax-b|| + lambda R(x)
-
-[verbose,x,QualMeasOpts,gpuids]=parse_inputs(proj,geo,angles,varargin);
+[verbose,x0,QualMeasOpts,gpuids,lambda]=parse_inputs(proj,geo,angles,varargin);
+x=x0;
 
 niter_outer = 15;
 
 measurequality=~isempty(QualMeasOpts);
 qualMeasOut=zeros(length(QualMeasOpts),niter*niter_outer);
-errorL2=zeros(1,niter*niter_outer);
+resL2=zeros(1,niter*niter_outer);
 
 for iii = 1:niter_outer
 
@@ -80,14 +59,13 @@ for iii = 1:niter_outer
 
     % Malena: We could avoid re-doing this, saving the initialisation
     % Just remember that we need cold restarts
-    [verbose,x,QualMeasOpts,gpuids] = parse_inputs(proj,geo,angles,varargin);
-
+    x=x0;
+    
     prox_aux_1 = Ax(x,geo,angles,'Siddon','gpuids',gpuids);
-    prox_aux_2 = Lx(W, x);
-    prox_aux_2 = cellfun(@(x) x*(sqrt(lambda)),prox_aux_2,'un',0);
-
+    prox_aux_2 = Lx(W, x)*sqrt(lambda);
+    
     r_aux_1 = proj - prox_aux_1;
-    r_aux_2 = cellfun(@(x) x*(-1),prox_aux_2,'un',0);
+    r_aux_2 = -prox_aux_2;
 
     % Malena: changed the format, r_aux_2 is 3
     % r = cat(3,r_aux_1, r_aux_2); % Malena: size guide, erase later, N x N x (100 + N-1)
@@ -103,39 +81,29 @@ for iii = 1:niter_outer
         if (ii==1 && verbose);tic;end
 
         q_aux_1 = Ax(p ,geo,angles,'Siddon','gpuids',gpuids);
-        q_aux_2 = Lx (W, p);
-        q_aux_2 = cellfun(@(x) x*sqrt(lambda),q_aux_2,'un',0);
+        q_aux_2 = Lx (W, p)*sqrt(lambda);
 
         % q = cat(3, q_aux_1, q_aux_2{1},q_aux_2{2},q_aux_2{3}); % Probably never need to actually do this
         % alpha=gamma/norm(q(:),2)^2;
-        alpha=gamma/(norm(q_aux_1(:))^2 + norm(q_aux_2{1}(:))^2 + norm(q_aux_2{2}(:))^2 + norm(q_aux_2{3}(:))^2);
+        alpha=gamma/(norm(q_aux_1(:))^2 + im3Dnorm(q_aux_2(:,:,:,1),'L2')^2 + im3Dnorm(q_aux_2(:,:,:,2),'L2')^2 + im3Dnorm(q_aux_2(:,:,:,3),'L2')^2);
 
 
         x=x+alpha*p;
-        x_out{iii}=x;
+        x_out=x;
         aux=proj-Ax(x,geo,angles,'Siddon','gpuids',gpuids);   % expensive, is there any way to check this better?
         % residual norm or the original least squares (not Tikhonov).
         % Think if that is what we want of the NE residual
 
-        errorL2((iii-1)*niter+ii)=im3Dnorm(aux,'L2');
+        resL2((iii-1)*niter+ii)=im3Dnorm(aux,'L2');
 
         if measurequality
             qualMeasOut(:,(iii-1)*niter+ii)=Measure_Quality(x0,x,QualMeasOpts);
         end
 
-        % This might happen now, we are minimizing the normal eqns residual
-%         if ii>1 && errorL2(ii)>errorL2(ii-1)
-%             % OUT!
-%             x=x-alpha*p;
-%             if verbose
-%                 disp(['CGLS stoped in iteration N', num2str(ii),' due to divergence.'])
-%             end
-%             return;
-%         end
 
         % If step is adecuate, then continue withg CGLS
         r_aux_1 = r_aux_1-alpha*q_aux_1;
-        r_aux_2 = cellfun(@(a,b)a-alpha*b, r_aux_2, q_aux_2, 'uni',0);
+        r_aux_2=r_aux_2-alpha*q_aux_2;
 
         s_aux_1 = Atb(r_aux_1 ,geo,angles,'matched','gpuids',gpuids);
         s_aux_2 =  sqrt(lambda) * Ltx (W, r_aux_2);
@@ -148,7 +116,7 @@ for iii = 1:niter_outer
 
         if (iii==1 && ii ==1 && verbose)
             expected_time=toc*niter*niter_outer;
-            disp('CGLS');
+            disp('ISN_TV_CGLS');
             disp(['Expected duration   :    ',secs2hms(expected_time)]);
             disp(['Expected finish time:    ',datestr(datetime('now')+seconds(expected_time))]);
             disp('');
@@ -182,9 +150,13 @@ end
 function W = build_weights ( x)
     % Directional discrete derivatives 
     % Reflective boundary conditions 
-    Dxx=cat(1,x(1:end-1,:,:)-x(2:end,:,:),zeros(1,size(x,2),size(x,3)));
-    Dyx=cat(2,x(:,1:end-1,:)-x(:,2:end,:),zeros(size(x,1),1,size(x,3)));
-    Dzx=cat(3,x(:,:,1:end-1)-x(:,:,2:end),zeros(size(x,1),size(x,2),1));
+    Dxx=zeros(size(x),'single');
+    Dyx=zeros(size(x),'single');
+    Dzx=zeros(size(x),'single');
+    
+    Dxx(1:end-1,:,:)=x(1:end-1,:,:)-x(2:end,:,:);
+    Dyx(:,1:end-1,:)=x(:,1:end-1,:)-x(:,2:end,:);
+    Dzx(:,:,1:end-1)=x(:,:,1:end-1)-x(:,:,2:end);
 
     W = (Dxx.^2+Dyx.^2+Dzx.^2+1e-6).^(-1/4); % Fix this...
 
@@ -193,40 +165,48 @@ end
 function out = Lx (W, x)
     % Directional discrete derivatives
     % Reflective boundary conditions 
-    Dxx=cat(1,x(1:end-1,:,:)-x(2:end,:,:),zeros(1,size(x,2),size(x,3)));
-    Dyx=cat(2,x(:,1:end-1,:)-x(:,2:end,:),zeros(size(x,1),1,size(x,3)));
-    Dzx=cat(3,x(:,:,1:end-1)-x(:,:,2:end),zeros(size(x,1),size(x,2),1));
-
+	Dxx=zeros(size(x),'single');
+    Dyx=zeros(size(x),'single');
+    Dzx=zeros(size(x),'single');
+    
+    Dxx(1:end-1,:,:)=x(1:end-1,:,:)-x(2:end,:,:);
+    Dyx(:,1:end-1,:)=x(:,1:end-1,:)-x(:,2:end,:);
+    Dzx(:,:,1:end-1)=x(:,:,1:end-1)-x(:,:,2:end);
     % Build weights - is it better to find the right rotation and add
     % tensors?
-    WDxx = W .* Dxx;
-    WDyx = W .* Dyx;
-    WDzx = W .* Dzx;
-
-    out = {WDxx WDyx WDzx};
+    out = cat(4,W .* Dxx, W .* Dyx, W .* Dzx);
 end
 
-function out = Ltx (W, x) % Input is a cell 
-    Wx_1 = W .* x{1};
-    Wx_2 = W .* x{2};
-    Wx_3 = W .* x{3};
+function out = Ltx (W, x)
+    Wx_1 = W .* x(:,:,:,1);
+    Wx_2 = W .* x(:,:,:,2);
+    Wx_3 = W .* x(:,:,:,3);
 
     % Left here, but this is how to make a transpose
-    DxtWx_1=cat(1,Wx_1(1,:,:),Wx_1(2:end-1,:,:)-Wx_1(1:end-2,:,:),-Wx_1(end-1,:,:));
-    DytWx_2=cat(2,Wx_2(:,1,:),Wx_2(:,2:end-1,:)-Wx_2(:,1:end-2,:),-Wx_2(:,end-1,:));
-    DztWx_3=cat(3,Wx_3(:,:,1),Wx_3(:,:,2:end-1)-Wx_3(:,:,1:end-2),-Wx_3(:,:,end-1));
+    DxtWx_1=Wx_1;
+    DytWx_2=Wx_2;
+    DztWx_3=Wx_3;
+    
+    DxtWx_1(2:end-1,:,:)=Wx_1(2:end-1,:,:)-Wx_1(1:end-2,:,:);
+    DxtWx_1(end,:,:)=-Wx_2(end-1,:,:);
+    
+    DytWx_2(:,2:end-1,:)=Wx_2(:,2:end-1,:)-Wx_2(:,1:end-2,:);
+    DytWx_2(:,end,:)=-Wx_2(:,end-1,:);
+    
+    DztWx_3(:,:,2:end-1)=Wx_3(:,:,2:end-1)-Wx_3(:,:,1:end-2);
+    DztWx_3(:,:,end)=-Wx_3(:,:,end-1);
 
     out = DxtWx_1 + DytWx_2 + DztWx_3;
 end
 %% parse inputs'
-function [verbose,x,QualMeasOpts,gpuids]=parse_inputs(proj,geo,angles,argin)
-opts=     {'init','initimg','verbose','qualmeas','gpuids'};
+function [verbose,x,QualMeasOpts,gpuids,lambda]=parse_inputs(proj,geo,angles,argin)
+opts=     {'init','initimg','verbose','qualmeas','gpuids','lambda'};
 defaults=ones(length(opts),1);
 
 % Check inputs
 nVarargs = length(argin);
 if mod(nVarargs,2)
-    error('TIGRE:CGLS:InvalidInput','Invalid number of inputs')
+    error('TIGRE:IRN_TV_CGLS:InvalidInput','Invalid number of inputs')
 end
 
 % check if option has been passed as input
@@ -235,7 +215,7 @@ for ii=1:2:nVarargs
     if ~isempty(ind)
         defaults(ind)=0;
     else
-       error('TIGRE:CGLS:InvalidInput',['Optional parameter "' argin{ii} '" does not exist' ]); 
+       error('TIGRE:IRN_TV_CGLS:InvalidInput',['Optional parameter "' argin{ii} '" does not exist' ]); 
     end
 end
 
@@ -250,7 +230,7 @@ for ii=1:length(opts)
             jj=jj+1;
         end
         if isempty(ind)
-            error('TIGRE:CGLS:InvalidInput',['Optional parameter "' argin{jj} '" does not exist' ]); 
+            error('TIGRE:IRN_TV_CGLS:InvalidInput',['Optional parameter "' argin{jj} '" does not exist' ]); 
         end
         val=argin{jj};
     end
@@ -275,7 +255,7 @@ for ii=1:length(opts)
                 continue;
             end
             if isempty(x)
-               error('TIGRE:CGLS:InvalidInput','Invalid Init option') 
+               error('TIGRE:IRN_TV_CGLS:InvalidInput','Invalid Init option') 
             end
             % % % % % % % ERROR
         case 'initimg'
@@ -286,7 +266,7 @@ for ii=1:length(opts)
                 if isequal(size(val),geo.nVoxel')
                     x=single(val);
                 else
-                    error('TIGRE:CGLS:InvalidInput','Invalid image for initialization');
+                    error('TIGRE:IRN_TV_CGLS:InvalidInput','Invalid image for initialization');
                 end
             end
         %  =========================================================================
@@ -297,7 +277,7 @@ for ii=1:length(opts)
                 if iscellstr(val)
                     QualMeasOpts=val;
                 else
-                    error('TIGRE:CGLS:InvalidInput','Invalid quality measurement parameters');
+                    error('TIGRE:IRN_TV_CGLS:InvalidInput','Invalid quality measurement parameters');
                 end
             end
          case 'verbose'
@@ -316,8 +296,14 @@ for ii=1:length(opts)
             else
                 gpuids = val;
             end
+        case 'lambda'
+            if default
+                lambda=10;
+            else
+                lambda=val;
+            end
         otherwise 
-            error('TIGRE:CGLS:InvalidInput',['Invalid input name:', num2str(opt),'\n No such option in CGLS()']);
+            error('TIGRE:IRN_TV_CGLS:InvalidInput',['Invalid input name:', num2str(opt),'\n No such option in IRN_TV_CGLS()']);
     end
 end
 
