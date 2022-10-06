8973e9f49f17dfb52242b3fc8d7ae345a50eeea6
Ander Biguri <ander.biguri@gmail.com>
2022-10-06 12:12:13 +0100

    Fix #402

--- MATLAB/Algorithms/OS_SART.m
+++ MATLAB/Algorithms/OS_SART.m
@@ -91,17 +91,7 @@ end
 
 
 % Projection weight, W
-geoaux=geo;
-geoaux.sVoxel([1 2])=geo.sVoxel([1 2])*1.1; % a bit bigger, to avoid numerical division by zero (small number)
-geoaux.sVoxel(3)=max(geo.sDetector(2),geo.sVoxel(3)); % make sure lines are not cropped. One is for when image is bigger than detector and viceversa
-geoaux.nVoxel=[2,2,2]'; % accurate enough?
-geoaux.dVoxel=geoaux.sVoxel./geoaux.nVoxel;
-W=Ax(ones(geoaux.nVoxel','single'),geoaux,angles,'Siddon','gpuids',gpuids);
-W(W<min(geo.dVoxel)/2)=Inf;
-W=1./W;
-
-% disp('Offset parameter size');
-% disp(size(geo.offDetector));
+W=computeW(geo,angles,gpuids);
 % Back-Projection weight, V
 V=computeV(geo,angles,alphablocks,orig_index,'gpuids',gpuids);
 
--- MATLAB/Algorithms/SIRT.m
+++ MATLAB/Algorithms/SIRT.m
@@ -67,15 +67,8 @@ end
 %% Create weighting matrices
 
 % Projection weight, W
-
-geoaux=geo;
-geoaux.sVoxel([1 2])=geo.sVoxel([1 2])*1.1; % a bit bigger, to avoid numerical division by zero (small number)
-geoaux.sVoxel(3)=max(geo.sDetector(2),geo.sVoxel(3)); % make sure lines are not cropped. One is for when image is bigger than detector and viceversa
-geoaux.nVoxel=[2,2,2]'; % accurate enough?
-geoaux.dVoxel=geoaux.sVoxel./geoaux.nVoxel;
-W=Ax(ones(geoaux.nVoxel','single'),geoaux,angles,'Siddon','gpuids',gpuids);
-W(W<min(geo.dVoxel)/4)=Inf;
-W=1./W;
+% Projection weight, W
+W=computeW(geo,angles,gpuids);
 
 % disp('Size of W matrix');
 % disp(size(W));
