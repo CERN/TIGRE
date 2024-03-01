2f0ea608e6022eff672f8acf72b78ed2480dd98f
AnderBiguri <ander.biguri@gmail.com>
2024-03-01 18:41:06 +0000

    Reformated input parsing to DE

--- MATLAB/Utilities/DE/DeDecompose.m
+++ MATLAB/Utilities/DE/DeDecompose.m
@@ -1,4 +1,4 @@
-function [Al_thickness,PMMA_thickness, angles_out] = DeDecompose(proj_l, angles_l, proj_h, angles_h, varargin)
+function [Al_thickness,PMMA_thickness, angles_out] = DeDecompose(proj_l, angles_l , proj_h, angles_h, varargin)
 %DEDECOMPSE Maps CBCT projections taken at two kVp settings to equivalent
 %thicknesses of Al and PMMA using precalibrated transfer functions.
 %   Coefficients C and D are the coefficients for the third-degree transfer
@@ -12,38 +12,33 @@ function [Al_thickness,PMMA_thickness, angles_out] = DeDecompose(proj_l, angles_
 %
 %   Proj_h and angles_h are the projection and angle data for the high-kV
 %   scan
-%  
+%
 %   Optional Arguments
-%    
+%       scanner - default VarianTrueVeam
+%
 %       kVl, kVh - the kV settings for the low- and high-kV scans,
 %       respectively.  defaults: 80, 140
-%       
+%
 %       interp_model - model for sinogram interpolation
-%            
-%           'nearest' - nearest neighbor (default).  
+%
+%           'nearest' - nearest neighbor (default).
 %
 %           'skip' - same as 'nearest, but skips projections without close
 %           matches.  Requires setting 'tolerance' value.
-%           
+%
 %           'linear' - linear interpolation (unreliable for large
 %           projection separations)
-%   
+%
 %   Coded by: Andrew Keeler
 %   Contact: akeeler@luc.edu
 %   Date: February 28, 2024
 
 % Load calibrated coefficients for transfer functions
 
-if ~ isfile('./de_cal.mat')
-    warning('Using DEMO Dual-energy transfer calibration. We recommend creating your own and storing it in TIGRE/Common/data/de_cal.mat')
-    load('./demo_de_cal.mat', 'de_cal');
-else
-    load('./de_cal.mat', 'de_cal');
-end
 
 [calibration, model, tolerance] = parse_inputs(varargin{:});
 
-tmp = getfield(de_cal, calibration);
+tmp = de_cal.(calibration);
 
 c = tmp.Al;
 d = tmp.PMMA;
@@ -65,8 +60,8 @@ for k = 1:size(proj_h,3)
     end
 
     L_array = proj_interp(proj_l, angles_l, angles_h(k), model, tolerance);
-    
-    if(L_array == NaN)
+
+    if(isnan(L_array))
         fprintf("No matches for projection %d.  Skipping.\n", k);
         skipcount = skipcount+1;
         continue;
@@ -75,7 +70,7 @@ for k = 1:size(proj_h,3)
     % map pixels from H and L to equivalent thicknesses
     for j = 1:size(proj_h,2)
         for i = 1:size(proj_h,1)
-            H = proj_h(i,j,k);  
+            H = proj_h(i,j,k);
             L = L_array(i,j);
             Al_thickness(i,j,k-skipcount) = c(1)*L + c(2)*H + c(3)*(L.^2) + c(4)*L.*H + c(5)*(H.^2) + c(6)*(L.^3) + c(7)*(H.*(L.^2)) + c(8)*(L.*(H.^2)) + c(9)*(H.^3);
             PMMA_thickness(i,j,k-skipcount) = d(1)*L + d(2)*H + d(3)*(L.^2) + d(4)*L.*H + d(5)*(H.^2) + d(6)*(L.^3) + d(7)*(H.*(L.^2)) + d(8)*(L.*(H.^2)) + d(9)*(H.^3);
@@ -85,36 +80,85 @@ for k = 1:size(proj_h,3)
 end
 end
 
-function [calibration, model, tolerance] = parse_inputs(varargin)
-    opts = {'kVl', 'kVh', 'model', 'tolerance'};
-    args = {80, 140, 'nearest', 1};
+function [scanner,calibration_pair, model, tolerance] = parse_inputs(argin)
+opts = {'scanner','kVl', 'kVh', 'interpolation', 'tolerance'};
+defaults=ones(length(opts),1);
 
-    % Check inputs
-    nVarargs = length(varargin);
-    if mod(nVarargs,2)
-        error('TIGRE:FDK:InvalidInput','Invalid number of inputs')
+% check if option has been passed as input
+for ii=1:2:nVarargs
+    ind=find(ismember(opts,lower(argin{ii})));
+    if ~isempty(ind)
+        defaults(ind)=0;
     end
+end
 
-    % check if option has been passed as input, assign new value if it has
-    for ii=1:2:nVarargs
-        ind=find(ismember(opts,varargin{ii}));
-        if ~isempty(ind)
-            args{ind}=varargin{ii+1};
+for ii=1:length(opts)
+    opt=opts{ii};
+    default=defaults(ii);
+    % if one option is not default, then extract value from input
+    if default==0
+        ind=double.empty(0,1);jj=1;
+        while isempty(ind)
+            ind=find(isequal(opt,lower(argin{jj})));
+            jj=jj+1;
         end
+
+        if isempty(ind)
+            error('TIGRE:DEDecompose:InvalidInput',['Optional parameter "' argin{jj} '" does not exist' ]);
+        end
+        val=argin{jj};
     end
 
-    for jj = 1:length(opts)
-        opt = opts{jj};
-        switch opt
-            case 'kVl'
-                kVl = args{jj};
-            case 'kVh'
-                    kVh = args{jj};
-            case 'model'
-                    model = args{jj};
-            case 'tolerance'
-                    tolerance = args{jj};
-        end
+    switch opt
+        case 'scanner'
+            if default
+                warning("You have not given a scanner for DE calibration.\n Dual Energy Calibration is strictly dependant on scanner calibration, so do not use the results here for quantitative analysis.\n Assuming VarianTrueBeam")
+                scanner='VarianTrueBeam';
+            else
+                if ~ischar( val)
+                    error('TIGRE:DEDecompose:InvalidInput','Scanner must be a string')
+                end
+                scanner=val;
+            end
+        case 'kVl'
+            if default
+                kVl=80;
+            else
+                if ~isscalar(val)
+                    error('TIGRE:DEDecompose:InvalidInput','KVl must be a scalar')
+                end
+                kVl=val;
+            end
+        case 'kVh'
+            if default
+                kVh=80;
+            else
+                if ~isscalar(val)
+                    error('TIGRE:DEDecompose:InvalidInput','KVh must be a scalar')
+                end
+                kVh=val;
+            end
+        case 'interpolation'
+             if default
+                model='linear';
+            else
+                if ~ischar( val)
+                    error('TIGRE:DEDecompose:InvalidInput','Interpolation must be a string')
+                end
+                model=val;
+             end
+        case 'tolerance'
+            if default
+                tolerance=1;
+            else
+                if ~isnumeric(val)
+                    error('TIGRE:DEDecompose:InvalidInput','tolerance must be a number')
+                end
+                tolerance=val;
+            end
+        otherwise
+            error('TIGRE:DEDecompose:InvalidInput',['Invalid input name:', num2str(opt),'\n No such option in DEDecompose()']);
     end
-    calibration = "V_"+num2str(kVl)+"_"+num2str(kVh);
+end
+calibration_pair=['V_', int2str(kVl), '_' , int2str(kVh)];
 end
