da28e83b8468fd355d47439d03088c9028f052fe
Emilien Valat <emilienvalat@gmail.com>
2024-07-01 16:11:10 +0200

    pytorch bindings with CPU/GPU pingpong + associated demo

---
+++ Python/tigre/utilities/pytorch_bindings.py
@@ -0,0 +1,161 @@
+import tigre
+
+import torch
+import numpy as np
+
+class AXFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        input_tensor:torch.Tensor, 
+        geo, 
+        angles, 
+        gpuids
+        ):
+        input_dimension = input_tensor.size()
+        device = input_tensor.device
+        input_tensor = input_tensor.detach().cpu().numpy()
+        assert input_dimension[1] == 1, f'The input tensor has a its first dimension equal to {input_dimension[1]}. Currently, only tensors with channel dimension = 1 are supported.'
+        if len(input_dimension) == 5:
+            input_tensor = input_tensor.squeeze(1)
+        result = []
+        for batch_index in range(input_tensor.shape[0]):
+            ### This is a hack.
+            ### TIGRE expects an array of shape [NVoxDepth, NVoxHeight, NVoxWidth]
+            ### Pytorch provides an array of shape 
+            #   -> [BatchSize, NChannels = 1, NVoxHeight, NVoxWidth] for 2D Tomography
+            #   -> [BatchSize, NChannels = 1, NVoxDepth, NVoxHeight, NVoxWidth] for 3D Tomography
+            # How we handle 2D case: we assume the channel dimension (=1) for PyTorch is the NVoxDepth (=1) dimension for TIGRE
+            # How we handle 3D case: we squeeze the input tensor to remove the channels dimension 
+            ax:np.ndarray = tigre.Ax(
+                    input_tensor[batch_index], 
+                    geo,
+                    angles,
+                    gpuids = gpuids)
+            # Ax returns an array of size [N_angles, DetX, DetY]
+            # For 2D, DetX = 1, and we want to output a tensor of size [BatchSize, NChannels = 1, N_angles, DetY]
+            # We then use the fact that DetX = 1 and transpose the output array
+            if len(input_dimension) == 4:
+                ax = ax.transpose((1,0,2))
+            result.append(
+                ax
+                )
+        result = torch.tensor(np.stack(result), requires_grad=True).to(device)
+        if len(input_dimension) == 5:
+            result = result.unsqueeze(1)
+        return result
+
+    @staticmethod
+    def setup_context(ctx, inputs, output):
+        input, geo, angles, gpuids = inputs
+        ctx.geo = geo
+        ctx.angles = angles
+        ctx.gpuids = gpuids
+
+    @staticmethod
+    def backward(ctx, grad_output:torch.Tensor):
+        device = grad_output.device
+        input_dimension = grad_output.size()
+        grad_output : np.ndarray= grad_output.detach().cpu().numpy()
+        if len(input_dimension) == 5:
+            grad_output = grad_output.squeeze(1)
+        elif len(input_dimension) == 4:
+            grad_output = grad_output.transpose((0,2,1,3))
+        result = []
+        for batch_index in range(grad_output.shape[0]):
+            atb : np.ndarray = tigre.Atb(
+                    grad_output[batch_index],
+                    ctx.geo,
+                    ctx.angles,
+                    gpuids = ctx.gpuids)
+            result.append(atb)
+        result = torch.tensor(np.stack(result), requires_grad=True).to(device)
+        if len(input_dimension) == 5:
+            result = result.unsqueeze(1)
+        return result, None, None, None
+    
+class ATBFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(input_tensor:torch.Tensor, geo, angles, gpuids):
+        device = input_tensor.device
+        input_dimension = input_tensor.size()
+        input_tensor:np.ndarray = input_tensor.detach().cpu().numpy()
+        if len(input_dimension) == 5:
+            input_tensor = input_tensor.squeeze(1)
+        elif len(input_dimension) == 4:
+            input_tensor = input_tensor.transpose((0,2,1,3))
+        result = []
+        for batch_index in range(input_tensor.shape[0]):
+            atb : np.ndarray = tigre.Atb(
+                    input_tensor[batch_index],
+                    geo,
+                    angles,
+                    gpuids = gpuids)
+            result.append(atb)
+        result = torch.tensor(np.stack(result), requires_grad=True).to(device)
+
+        if len(input_dimension) == 5:
+            result = result.unsqueeze(1)
+
+        return result
+
+    @staticmethod
+    def setup_context(ctx, inputs, output):
+        input, geo, angles, gpuids = inputs
+        ctx.geo = geo
+        ctx.angles = angles
+        ctx.gpuids = gpuids
+
+    @staticmethod
+    def backward(ctx, grad_output:torch.Tensor):
+        input_dimension = grad_output.size()
+        device = grad_output.device
+        grad_output = grad_output.detach().cpu().numpy()
+        assert input_dimension[1] == 1, f'The input tensor has a its first dimension equal to {input_dimension[1]}. Currently, only tensors with channel dimension = 1 are supported.'
+        if len(input_dimension) == 5:
+            grad_output = grad_output.squeeze(1)
+        result = []
+        for batch_index in range(grad_output.shape[0]):
+            ### This is a hack.
+            ### TIGRE expects an array of shape [NVoxDepth, NVoxHeight, NVoxWidth]
+            ### Pytorch provides an array of shape 
+            #   -> [BatchSize, NChannels = 1, NVoxHeight, NVoxWidth] for 2D Tomography
+            #   -> [BatchSize, NChannels = 1, NVoxDepth, NVoxHeight, NVoxWidth] for 3D Tomography
+            # How we handle 2D case: we assume the channel dimension (=1) for PyTorch is the NVoxDepth (=1) dimension for TIGRE
+            # How we handle 3D case: we squeeze the input tensor to remove the channels dimension 
+            ax:np.ndarray = tigre.Ax(
+                    grad_output[batch_index], 
+                    ctx.geo,
+                    ctx.angles,
+                    gpuids = ctx.gpuids)
+            # Ax returns an array of size [N_angles, DetX, DetY]
+            # For 2D, DetX = 1, and we want to output a tensor of size [BatchSize, NChannels = 1, N_angles, DetY]
+            # We then use the fact that DetX = 1 and transpose the output array
+            if len(input_dimension) == 4:
+                ax = ax.transpose((1,0,2))
+            result.append(
+                ax
+                )
+        result = torch.tensor(np.stack(result), requires_grad=True).to(device)
+        if len(input_dimension) == 5:
+            result = result.unsqueeze(1)
+        return result, None, None, None
+
+class AX(torch.nn.Module):
+    def __init__(self, geo, angles, gpuids):
+        super(AX, self).__init__()
+        self.geo = geo
+        self.angles = angles
+        self.gpuids = gpuids
+
+    def forward(self, x):
+        return AXFunction.apply(x, self.geo, self.angles, self.gpuids)
+    
+class ATB(torch.nn.Module):
+    def __init__(self, geo, angles, gpuids):
+        super(ATB, self).__init__()
+        self.geo = geo
+        self.angles = angles
+        self.gpuids = gpuids
+
+    def forward(self, x):
+        return ATBFunction.apply(x, self.geo, self.angles, self.gpuids)
