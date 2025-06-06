c4223b9519d903daea8c3825aed0fd302aa56ba9
defThu <167285267+defThu@users.noreply.github.com>
2025-06-06 22:51:29 +0800

    Update Ax_mex.cpp
    fix bug caused by offSource

--- MATLAB/Utilities/cuda_interface/Ax_mex.cpp
+++ MATLAB/Utilities/cuda_interface/Ax_mex.cpp
@@ -141,7 +141,7 @@ void mexFunction(int  nlhs , mxArray *plhs[],
     mxArray * geometryMex=(mxArray*)prhs[1];
     
     // IMPORTANT-> Make sure Matlab creates the struct in this order.
-    const char *fieldnames[14];
+    const char *fieldnames[15];
     fieldnames[0] = "nVoxel";
     fieldnames[1] = "sVoxel";
     fieldnames[2] = "dVoxel";
@@ -171,7 +171,7 @@ void mexFunction(int  nlhs , mxArray *plhs[],
     geo.unitX=1;geo.unitY=1;geo.unitZ=1;
     bool coneBeam=true;
 //     mexPrintf("%d \n",nfields);
-    for(int ifield=0; ifield<14; ifield++) {
+    for(int ifield=0; ifield<15; ifield++) {
         tmp=mxGetField(geometryMex,0,fieldnames[ifield]);
         if(tmp==NULL){
             //tofix
